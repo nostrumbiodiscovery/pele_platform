18cea1ba7301927f66e78b18b10644a180325048
Daniel Soler Viladrich <daniel.soler@nostrumbiodiscovery.com>
2017-09-08 14:28:44 +0200

    make_lib tes half implemented

--- test/test_PlopRotTemp.py
+++ test/test_PlopRotTemp.py
@@ -6,12 +6,11 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import PlpRotTemp.PlopRotTemp as pl
 #from ciphers.DESchiper import convert_to_binary, DES, XOR
 
-SCHRODINGER_PYTHON = "/opt/schrodinger2016-4/utilities/python"
+TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
 MAE_FILE = 'ain.mae'
 ROOT = 'ain'
-TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
-MAE_CONVERSION = '[1,2,3,4,5,14,6,15,7,16,8,17,9,10,11,12,13,18,19,20]'
-TEMPLATE_CONVERSION = '[1,2,3,4,5,7,9,11,13,14,15,16,17,6,8,10,12,18,19,20]'
+MAE_CONVERSION = [0,1,2,3,4,13,5,14,6,15,7,16,8,9,10,11,12,17,18,19]
+TEMPLATE_CONVERSION = [0,1,2,3,4,6,8,10,12,13,14,15,16,5,7,9,11,17,18,19]
 
 @pytest.mark.parametrize("argument, expected", [
                          ('ain.mae', 'ain'),
@@ -31,26 +30,41 @@ def test_build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, ex
     assert template_file == expected
 
 """
-@pytest.mark.parametrize("MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected", [
+@pytest.mark.parametrize("mae_min_file, user_fixed_bonds, log_file, use_rings, \
+                          use_mult_lib, user_core_atom, user_tors, back_tors, \
+                          max_tors, R_group_root_atom_name", 
+                          [
                          (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
                          (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
                          ])
 
 def test_FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
                  use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name):
-   [mae_num, parent, rank, tors, use_rings, group, back_tors, tors_ring_num] = \
+    [mae_num, parent, rank, tors, use_rings, group, back_tors, tors_ring_num] = \
         pl.FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
                  use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name)
     assert template_file == expected
 """
 
+
 @pytest.mark.parametrize("mae_file, template_file, mae_expected, template_expected", [
-                         (os.path.join(TEST_PATH, 'ain.mae'), os.path.join(TEST_PATH, 'ain.mae'), MAE_CONVERSION, TEMPLATE_CONVERSION),
+                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain.hetgrp_ffgen' , MAE_CONVERSION, TEMPLATE_CONVERSION),
                          ])
 def test_MatchTempMaeAtoms(mae_file, template_file, mae_expected, template_expected):
-    [mae2temp, temp2mae] = pl.MatchTempMaeAtoms(mae_file, template_file)
-    assert mae2temp == mae_expected
-    assert temp2mae == template_expected
+    [mae2temp, temp2mae] = pl.make_libraries(mae_file, template_file)
+    assert mae2temp == template_expected
+    assert temp2mae == mae_expected
 
+"""###############testttt
+@pytest.mark.parametrize("mae_file, template_file, mae_expected, template_expected", [
+                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain.hetgrp_ffgen' , MAE_CONVERSION, TEMPLATE_CONVERSION),
+                         ])
+def test_make_libraries(mae_file, template_file, mae_expected, template_expected):
+    rotamers_file = pl.make_libraries(resname, conf_file, root, names, zmat_atoms,
+                                      group, use_rings, use_mult_lib,
+                                      output_template_file, gridres, debug)
+    assert mae2temp == template_expected
+    assert temp2mae == mae_expected
+"""
 
 
