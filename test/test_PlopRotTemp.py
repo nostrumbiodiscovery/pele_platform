918213c4069c24a79c7e0cb4597e9cddaf070507
Daniel Soler Viladrich <daniel.soler@nostrumbiodiscovery.com>
2017-09-08 12:46:25 +0200

    build template & Match Template

--- test/test_PlopRotTemp.py
+++ test/test_PlopRotTemp.py
@@ -9,7 +9,9 @@ import PlpRotTemp.PlopRotTemp as pl
 SCHRODINGER_PYTHON = "/opt/schrodinger2016-4/utilities/python"
 MAE_FILE = 'ain.mae'
 ROOT = 'ain'
-TEST_PATH = os.path.dirname(__file__)
+TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
+MAE_CONVERSION = '[1,2,3,4,5,14,6,15,7,16,8,17,9,10,11,12,13,18,19,20]'
+TEMPLATE_CONVERSION = '[1,2,3,4,5,7,9,11,13,14,15,16,17,6,8,10,12,18,19,20]'
 
 @pytest.mark.parametrize("argument, expected", [
                          ('ain.mae', 'ain'),
@@ -25,7 +27,30 @@ def test_get_root(argument, expected):
                          (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
                          ])
 def test_build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected):
-    print(MAE_FILE)
     [template_file, output_template_file, mae_file_hetgrp_ffgen, files, resname] = pl.build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name)
     assert template_file == expected
+
+"""
+@pytest.mark.parametrize("MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected", [
+                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
+                         (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
+                         ])
+
+def test_FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
+                 use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name):
+   [mae_num, parent, rank, tors, use_rings, group, back_tors, tors_ring_num] = \
+        pl.FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
+                 use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name)
+    assert template_file == expected
+"""
+
+@pytest.mark.parametrize("mae_file, template_file, mae_expected, template_expected", [
+                         (os.path.join(TEST_PATH, 'ain.mae'), os.path.join(TEST_PATH, 'ain.mae'), MAE_CONVERSION, TEMPLATE_CONVERSION),
+                         ])
+def test_MatchTempMaeAtoms(mae_file, template_file, mae_expected, template_expected):
+    [mae2temp, temp2mae] = pl.MatchTempMaeAtoms(mae_file, template_file)
+    assert mae2temp == mae_expected
+    assert temp2mae == template_expected
+
+
 
