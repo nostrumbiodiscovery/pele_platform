9ed8397bbb66b2e4520c808680a29db0cfb25544
Daniel Soler Viladrich <daniel.soler@nostrumbiodiscovery.com>
2017-09-15 11:49:57 +0200

    test_search_parent_atom

--- test/test_PlopRotTemp.py
+++ test/test_PlopRotTemp.py
@@ -11,6 +11,12 @@ MAE_FILE = 'ain.mae'
 ROOT = 'ain'
 MAE_CONVERSION = [0,1,2,3,4,13,5,14,6,15,7,16,8,9,10,11,12,17,18,19]
 TEMPLATE_CONVERSION = [0,1,2,3,4,6,8,10,12,13,14,15,16,5,7,9,11,17,18,19]
+BONDS = [[0, 1], [1, 2], [1, 3], [3, 4], [3, 8], [4, 5], [4, 13], [5, 6], 
+        [5, 14], [6, 7], [6, 15], [7, 8], [7, 16], [8, 9], [9, 10], [10, 11],
+        [10, 12], [12, 17], [12, 18], [12, 19]]
+PARENT_RESULT = [0, 1, 2, 2, 4, 5, 6, 7, 4, 9, 10, 11, 11, 5, 6, 7, 8, 13, 13, 13]
+
+
 
 @pytest.mark.parametrize("argument, expected", [
                          ('ain.mae', 'ain'),
@@ -25,26 +31,11 @@ def test_get_root(argument, expected):
                          (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
                          (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
                          ])
-def test_build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected):
-    [template_file, output_template_file, mae_file_hetgrp_ffgen, files, resname] = pl.build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name)
-    assert template_file == expected
+# def test_build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected):
+#     [template_file, output_template_file, mae_file_hetgrp_ffgen, files, resname] = pl.build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name)
+#     assert template_file == expected
 
-"""
-@pytest.mark.parametrize("mae_min_file, user_fixed_bonds, log_file, use_rings, \
-                          use_mult_lib, user_core_atom, user_tors, back_tors, \
-                          max_tors, R_group_root_atom_name", 
-                          [
-                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
-                         (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
-                         ])
 
-def test_FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
-                 use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name):
-    [mae_num, parent, rank, tors, use_rings, group, back_tors, tors_ring_num] = \
-        pl.FindCore(mae_min_file, user_fixed_bonds, log_file, use_rings, \
-                 use_mult_lib, user_core_atom, user_tors, back_tors, max_tors, R_group_root_atom_name)
-    assert template_file == expected
-"""
 
 
 @pytest.mark.parametrize("mae_file, template_file, mae_expected, template_expected", [
@@ -72,7 +63,7 @@ def test_check_repite_names(mae_file, expected):
 @pytest.mark.parametrize("rotamer_library", [
                          (os.path.join(TEST_PATH,'ain_vdw')),
                          ])
-def test_check_repite_names(rotamer_library):
+def test_check_replace_vdwr(rotamer_library):
     pl.replace_vdwr_from_library(rotamer_library)
     radius_vdw_info, start_index, end_index = pl.parse_nonbonded(rotamer_library)
     for i, rdw_line in enumerate(radius_vdw_info):
@@ -81,17 +72,13 @@ def test_check_repite_names(rotamer_library):
         if(rdw== 0):
             assert 0
  
-
-"""###############testttt
-@pytest.mark.parametrize("mae_file, template_file, mae_expected, template_expected", [
-                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain.hetgrp_ffgen' , MAE_CONVERSION, TEMPLATE_CONVERSION),
+@pytest.mark.parametrize("bonds, n_atoms, expected", [
+                         (BONDS, 20, PARENT_RESULT),
                          ])
-def test_make_libraries(mae_file, template_file, mae_expected, template_expected):
-    rotamers_file = pl.make_libraries(resname, conf_file, root, names, zmat_atoms,
-                                      group, use_rings, use_mult_lib,
-                                      output_template_file, gridres, debug)
-    assert mae2temp == template_expected
-    assert temp2mae == mae_expected
-"""
+def test_search_parent_atom(bonds, n_atoms, expected):
+    parents = pl.search_parent_atom(bonds, n_atoms)
+    assert parents == expected
+    
+
 
 
