2ecf3e20ae478abfe833767887d9320911879ace
Daniel Soler Viladrich <daniel.soler@nostrumbiodiscovery.com>
2017-09-28 17:58:52 +0200

    test_PlopRotTemp() + parsetemplate()

--- test/test_PlopRotTemp.py
+++ test/test_PlopRotTemp.py
@@ -2,11 +2,12 @@ import pytest
 import sys
 import os
 import subprocess
+from test_config import parse_template
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import PlpRotTemp.PlopRotTemp as pl
 #from ciphers.DESchiper import convert_to_binary, DES, XOR
 
-TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
 MAE_FILE = 'ain.mae'
 ROOT = 'ain'
 MAE_CONVERSION = [0,1,2,3,4,13,5,14,6,15,7,16,8,9,10,11,12,17,18,19]
@@ -15,6 +16,14 @@ BONDS = [[0, 1], [1, 2], [1, 3], [3, 4], [3, 8], [4, 5], [4, 13], [5, 6],
         [5, 14], [6, 7], [6, 15], [7, 8], [7, 16], [8, 9], [9, 10], [10, 11],
         [10, 12], [12, 17], [12, 18], [12, 19]]
 PARENT_RESULT = [0, 1, 2, 2, 4, 5, 6, 7, 4, 9, 10, 11, 11, 5, 6, 7, 8, 13, 13, 13]
+REPOS_PATH = os.path.abspath(os.path.join(__file__ ,"../.."))
+TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
+MAIN_PATH =  os.path.join(REPOS_PATH, '/PlpRotTemp/main.py')
+OLD_MAIN_PATH = '/home/dani/repos/presentation/PlpRotTemp/main_16_9_17.py'
+try:
+    PYTHON_PATH = os.path.join(os.environ['SCHRODINGER'] + "/utilities/python")
+except KeyError:
+    print("Set SCHRODINGER environment variable path")
 
 
 
@@ -27,10 +36,10 @@ def test_get_root(argument, expected):
     root = pl.get_root_path(argument)
     assert root == expected
 
-@pytest.mark.parametrize("MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected", [
-                         (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
-                         (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
-                         ])
+# @pytest.mark.parametrize("MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected", [
+#                          (os.path.join(TEST_PATH, 'ain.mae'), 'ain', '2005', '', '', '', 'ain.hetgrp_ffgen'),
+#                          (os.path.join(TEST_PATH,'MI4.mae'), 'MI4', '2005', '', '', '', 'mi4.hetgrp_ffgen'),
+#                          ])
 # def test_build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name, expected):
 #     [template_file, output_template_file, mae_file_hetgrp_ffgen, files, resname] = pl.build_template(MAE_FILE, root, OPLS, hetgrp_opt, old_name, new_name)
 #     assert template_file == expected
@@ -72,13 +81,23 @@ def test_check_replace_vdwr(rotamer_library):
         if(rdw== 0):
             assert 0
  
-@pytest.mark.parametrize("bonds, n_atoms, expected", [
-                         (BONDS, 20, PARENT_RESULT),
+
+@pytest.mark.parametrize("input_file", [
+                         (MAE_FILE),
+                         ('MI4.mae')
                          ])
-def test_search_parent_atom(bonds, n_atoms, expected):
-    parents = pl.search_parent_atom(bonds, n_atoms)
-    assert parents == expected
-    
-
-
-
+def test_PlopRotTemp(input_file):
+    res_name = input_file.split('.')[0]
+    subprocess.call([PYTHON_PATH, MAIN_PATH, os.path.join(TEST_PATH, input_file)])
+    subprocess.call([PYTHON_PATH, OLD_MAIN_PATH, os.path.join(TEST_PATH, input_file)])
+    new_sections = parse_template(os.path.join(REPOS_PATH,res_name.upper()), res_name.upper())
+    old_sections = parse_template(os.path.join(REPOS_PATH,res_name), res_name.upper())
+    for i, (new_items, old_items) in enumerate(zip(new_sections, old_sections)):
+        for j, (new_item, old_item) in enumerate(zip(new_items, old_items)):
+            if(new_item != old_item):
+                print('SECTION {} {} \n'.format(i,j))
+                print(new_item)
+                print('\n')
+                print(old_item)
+                print('\n')
+    assert 1==2
