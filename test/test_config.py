f5425f2670728aa0c5fef21efa9f0723a7813e94
Daniel Soler Viladrich <daniel.soler@nostrumbiodiscovery.com>
2017-10-27 18:25:22 +0200

    test_class + absolute_value when hashing atoms to avoid problems with negative torsions for pele

--- test/test_config.py
+++ test/test_config.py
@@ -3,98 +3,168 @@ import os
 import re
 import subprocess
 
-MAE_FILE = 'ain.mae'
-ROOT = 'ain'
-MAE_CONVERSION = [0,1,2,3,4,13,5,14,6,15,7,16,8,9,10,11,12,17,18,19]
-TEMPLATE_CONVERSION = [0,1,2,3,4,6,8,10,12,13,14,15,16,5,7,9,11,17,18,19]
-BONDS = [[0, 1], [1, 2], [1, 3], [3, 4], [3, 8], [4, 5], [4, 13], [5, 6], 
-        [5, 14], [6, 7], [6, 15], [7, 8], [7, 16], [8, 9], [9, 10], [10, 11],
-        [10, 12], [12, 17], [12, 18], [12, 19]]
-PARENT_RESULT = [0, 1, 2, 2, 4, 5, 6, 7, 4, 9, 10, 11, 11, 5, 6, 7, 8, 13, 13, 13]
+NAMES = ['sigma', 'epsilon', 'charges', 'SGBr', 'vdwr', 'alpha', 'gamma']
+DEFAULT_RES_NAME = "LIG"
 REPOS_PATH = os.path.abspath(os.path.join(__file__ ,"../.."))
 TEST_PATH = os.path.join(os.path.dirname(__file__), 'data')
 MAIN_PATH =  os.path.join(REPOS_PATH, 'PlpRotTemp/main.py')
 OLD_MAIN_PATH = '/home/dani/repos/presentation/PlpRotTemp/main_16_9_17.py'
+TRESHOLD= 0.15
+
+
 try:
     PYTHON_PATH = os.path.join(os.environ['SCHRODINGER'] + "/utilities/python")
 except KeyError:
     print("Set SCHRODINGER environment variable path")
-TRESHOLD= 0.15
+
+class PlopTest:
+
+  def __init__(self, input_file):
+    self.input_file = input_file
+    self.old_names = None
+    self.new_names = None
+
+  def run_versions(self):
+
+    res_name = "LIG"
+
+    try:
+      os.remove("LIG".lower())
+      os.remove("LIG".upper())
+    except OSError:
+      pass
+
+    subprocess.call([PYTHON_PATH, MAIN_PATH, os.path.join(TEST_PATH, self.input_file)])
+    subprocess.call([PYTHON_PATH, OLD_MAIN_PATH, os.path.join(TEST_PATH, self.input_file)])
 
 
+    new_sections = self.parse_template(os.path.join(REPOS_PATH,res_name.upper()))
+    old_sections = self.parse_template(os.path.join(REPOS_PATH,res_name.lower()))
 
-def run_versions(input_file):
-  try:
-    os.remove("lig")
-    os.remove("LIG")
-  except OSError:
-    pass
-  res_name = "LIG"
-  NAMES = ['atom1', 'atom2', 'spring', 'eq_dist']
-  subprocess.call([PYTHON_PATH, MAIN_PATH, os.path.join(TEST_PATH, input_file)])
-  subprocess.call([PYTHON_PATH, OLD_MAIN_PATH, os.path.join(TEST_PATH, input_file)])
-  new_sections = parse_template(os.path.join(REPOS_PATH,res_name.upper()), res_name.upper())
-  old_sections = parse_template(os.path.join(REPOS_PATH,res_name.lower()), res_name.upper())
-  return new_sections, old_sections
+    self.old_names = old_sections[0][3]
+    self.new_names = new_sections[0][3]
 
-def parse_template(file_path, input_file):
-  
-  res_name = input_file.split('.')[0]
+    return old_sections, new_sections
 
-  starts = [False, False, False, False, False, False]
-  ends = [False, False, False, False, False, False]
-  KEYWORDS = [res_name, 'NBON', 'BOND', 'PHI', 'IPHI']
-  parents, sidechain, atom_types, names = ([] for i in range(4))
-  sigma, epsilon, charges, SGBr, vdwr, alpha, gamma = ([] for i in range(7))
-  atom1b, atom2b, springK, eq_dist = ([] for i in range(4))
-  atom1t, atom2t, atom3t, angles, force = ([] for i in range(5))
-  atom1p, atom2p, atom3p, atom4p, valuep, coefp, componentp = ([] for i in range(7))
-  atom1i, atom2i, atom3i, atom4i, valuei, coefi, componenti = ([] for i in range(7))
+  def parse_template(self, file_path):
 
-  ITEMS = [
-          [parents, sidechain, atom_types, names], 
-          [sigma, epsilon, charges, SGBr, vdwr, alpha, gamma],
-          [atom1b, atom2b, springK, eq_dist],
-          [atom1t, atom2t, atom3t, angles, force],
-          [atom1p, atom2p, atom3p, atom4p, valuep, coefp, componentp],
-          [atom1i, atom2i, atom3i, atom4i, valuei, coefi, componenti]
-          ]
+    res_name = "LIG"
 
-  INDEXES = [
-            [1,2,3,4],
-            [1,2,3,4,5,6,7],
-            [0,1,2,3],
-            [0,1,2,3,4],
-            [0,1,2,3,4,5,6],
-            [0,1,2,3,4,5,6]
+    parents, sidechain, atom_types, names = ([] for i in range(4))
+    sigma, epsilon, charges, SGBr, vdwr, alpha, gamma = ([] for i in range(7))
+    atom1b, atom2b, springK, eq_dist = ([] for i in range(4))
+    atom1t, atom2t, atom3t, angles, force = ([] for i in range(5))
+    atom1p, atom2p, atom3p, atom4p, valuep, coefp, componentp = ([] for i in range(7))
+    atom1i, atom2i, atom3i, atom4i, valuei, coefi, componenti = ([] for i in range(7))
+
+    ITEMS = [
+            [parents, sidechain, atom_types, names], 
+            [sigma, epsilon, charges, SGBr, vdwr, alpha, gamma],
+            [atom1b, atom2b, springK, eq_dist],
+            [atom1t, atom2t, atom3t, angles, force],
+            [atom1p, atom2p, atom3p, atom4p, valuep, coefp, componentp],
+            [atom1i, atom2i, atom3i, atom4i, valuei, coefi, componenti]
             ]
-  
 
-  with open(file_path, 'r') as f:
+    INDEXES = [
+              [1,2,3,4],
+              [1,2,3,4,5,6,7],
+              [0,1,2,3],
+              [0,1,2,3,4],
+              [0,1,2,3,4,5,6],
+              [0,1,2,3,4,5,6]
+              ]
     
-    line = f.readline() #Skip Commentaris
-    line = f.readline() #Skip Commentaris
-    line = f.readline() #Skip Commentaris
-    for i, (keyword, item, index)  in enumerate(zip(
-        KEYWORDS, ITEMS, INDEXES)):
-        line = re.sub(' +',' ',line)
-        line = line.strip('\n').strip()
-        while not ends[i]:
-            # print(line)
-            if line.startswith(keyword):
-              starts[i] = True
-            elif starts[i]:
-              line = line.split()
-              if len(line)==1:
-                ends[i] = True
-                line = line[0]
-                continue
-              else:
-                for k, j in zip(item, index):
-                    k.append(line[j])
-              
-            line = f.readline()
-    # print(ITEMS)
+    KEYWORDS = [res_name, 'NBON', 'BOND', 'THET', 'PHI', 'IPHI']
+    
+    starts = [False, False, False, False, False, False]
+    ends = [False, False, False, False, False, False]
+    print(file_path)
+    with open(file_path, 'r') as f:
+      line = f.readline() #Skip Commentaris
+      line = f.readline() #Skip Commentaris
+      line = f.readline() #Skip Commentaris
+      for i, (keyword, item, index)  in enumerate(zip(KEYWORDS, ITEMS, INDEXES)):
+          line = re.sub(' +',' ',line)
+          line = line.strip('\n').strip()
+          while not ends[i]:
+              if line.startswith(keyword):
+                starts[i] = True
+              elif starts[i]:
+                line = line.split()
+                if len(line)==1:
+                  ends[i] = True
+                  line = line[0]
+                  continue
+                else:
+                  for k, j in zip(item, index):
+                      k.append(line[j])
+                
+              line = f.readline()
     return ITEMS
+
+  
+  def compare_NBND_sections(self, old_sections, new_sections):
+
+      num_atoms = len(self.old_names)
+    
+      NBND_old_sect = old_sections[1]
+      NBND_new_sect = new_sections[1]
+
+      NBND_new_ordered = []
+      for i in range(num_atoms):
+        old_name = self.old_names[i].strip('_')
+        for j in range(num_atoms):
+          if(old_name == self.new_names[j]):
+            NBND_new_ordered.append([param[j] for param in NBND_new_sect])
+
+      translated_old_NBND = zip(*NBND_old_sect)
+
+      return translated_old_NBND, NBND_new_ordered
+
+  def compare_BND_sections(self, old_sections, new_sections, section):
+
+
+    SECTIONS = { 
+                 'BND' : ['BND', 2, 2],
+                 'TOR' : ['TOR', 3, 3],
+                 'PHI' : ['PHI', 4, 4],
+                 'IPHI' : ['IPHI', 5, 4]
+              }
+
+    section = SECTIONS[section]
+    section_name = section[0]
+    section_index = section[1]
+    section_elements = section[2]
+
+    BND_old_sect = old_sections[section_index]
+    BND_new_sect = new_sections[section_index]
+
+    if(len(BND_old_sect)!=BND_new_sect):
+      assert("Different number of bonds")
+   
+    #"Hashing atom numbers to atom names"
+    print("BBBBB")
+    for j, (old_atoms, new_atoms) in enumerate(zip(BND_old_sect[0:section_elements], BND_new_sect[0:section_elements])):
+      for i, (old_atom, new_atom) in enumerate(zip(old_atoms, new_atoms)):
+        old_atom = int(old_atom)
+        new_atom = int(new_atom)
+        # new_atom = int(new_atom)
+        # if(old_atom<0):
+        #   old_atom = abs(old_atom)
+        # if(new_atom<0):
+        #   new_atom = abs(new_atom)
+        BND_old_sect[j][i] = self.old_names[(old_atom)-1].strip('_')
+        BND_new_sect[j][i] = self.new_names[(new_atom)-1].strip('_')
+
+
+    BND_old_sect = zip(*BND_old_sect)
+    BND_new_sect = zip(*BND_new_sect)
+
+    return BND_old_sect, BND_new_sect
+
+
+
+
 
 
