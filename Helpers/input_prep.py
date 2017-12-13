eb5d410eb07aa94efc9537777193443fca8299d3
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2017-12-13 17:34:46 +0100

    Ligand atom type align (underscore related problem)

--- Helpers/input_prep.py
+++ Helpers/input_prep.py
@@ -5,23 +5,52 @@ class input_fixer(object):
 
     def __init__(self, input):
         self.input = input
-        self.strip_waters()
+        self.fix_input()
 
-    def strip_waters(self):
+    def fix_input(self):
+        
+        """
+        1- Strip Waters
+        2- Align Atoms
+        """
+
         input_fix = []
         with open(self.input, 'r') as pdb:
             for line in pdb:
-                if line.startswith("ATOM") or line.startswith("HETATM"):
+                if line.startswith("ATOM"):
+                    #Strip waters
                     if line[17:20] != "HOH":
                         input_fix.append(line)
-                    else:
-                        continue
+                elif line.startswith("HETATM"):
+                    #Strip waters and align atoms
+                    if line[17:20] != "HOH":
+                        line = self.align_atomnames(line)
+                        input_fix.append(line)
                 else:
+                    #Nothing
                     input_fix.append(line)
 
         with open(self.input, 'w') as fix_pdb:
             fix_pdb.write("".join(input_fix))
 
+    @staticmethod
+    def align_atomnames(line):
+        
+        """
+          Align atom names to the right to match
+          PlopRoTemp Template
+        
+        """
+        if line[12]!= " " and line[15] == " ": 
+            new_line = "{} {}{}".format(line[0:12], line[12:16], line[17:])
+            return new_line
+        else:
+            return line
+
+
+
+
+        
 
 if __name__ == '__main__':
     fixer = input_fixer(sys.argv[1])
