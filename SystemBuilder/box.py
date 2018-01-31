4af25781ecb31197acc7a4a49502512281e05a6d
Daniel Soler <daniel.soler@e-campus.uab.cat>
2018-02-01 00:32:33 +0100

    Tranform class to func for portability

--- SystemBuilder/box.py
+++ SystemBuilder/box.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append("/home/dsoler/PelePlop")
-import math
+import os
+import argparse
 from Adaptive import template_builder as tb
 
 KEYWORDS = ["CENTER_X", "CENTER_Y", "CENTER_Z", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8"]
@@ -8,50 +8,40 @@ COORD = "{:>11.3f}{:>8.3f}{:>8.3f}"
 CENTER = "{:.3f}"
 
 
-class BoxBuilder(tb.TemplateBuilder):
-    """
-       Create a box.pdb file with a cuadratic box
-       based on a sphere grid.
-    """
+def parse_args():
+    parser = argparse.ArgumentParser(description="Create pdb-box from center and radius")
+    parser.add_argument("center", nargs='+', type=int, help='center of the box')
+    parser.add_argument("radius", type=int, help='radius of the box')
+    parser.add_argument("-f", "--file", default="box.pdb", help='output file')
+    args = parser.parse_args()
+    return args.center, args.radius, args.file
 
-    def __init__(self, cm_receptor, cm_ligand, file):
-        self.cm_receptor = cm_receptor
-        self.cm_ligand = cm_ligand        
-        self.file = file
-        
 
-    def create(self):
-        """
-          Description: Create pele box by
-          defining center and size.
-        """
-        self.center = [(cm_r + cm_l)/2 for cm_r, cm_l in zip(self.cm_receptor, self.cm_ligand)]
-        cm_rx, cm_ry, cm_rz = self.cm_receptor
-        cm_lx, cm_ly, cm_lz = self.cm_ligand
-        self.r = (math.sqrt((cm_rx-cm_lx)**2 + (cm_ry-cm_ly)**2 + (cm_rz-cm_lz)**2)/2.0)+5
-        
-        
-    def build_box_pdb(self):
-        self.build_box_crd()
-        self.replace =  {keyword : value for keyword, value in zip(KEYWORDS, self.values)}
-        super(BoxBuilder, self).__init__(self.file, self.replace)	
+def build_box(center, radius, file):
 
-    def build_box_crd(self):
-        cx, cy, cz = self.center
-        self.v1 = COORD.format(cx-self.r,cy-self.r, cz-self.r)
-        self.v2 = COORD.format(cx+self.r,cy-self.r, cz-self.r)
-        self.v3 = COORD.format(cx+self.r,cy-self.r, cz+self.r)
-        self.v4 = COORD.format(cx-self.r,cy-self.r, cz+self.r)
-        self.v5 = COORD.format(cx-self.r,cy+self.r, cz-self.r)
-        self.v6 = COORD.format(cx+self.r,cy+self.r, cz-self.r)
-        self.v7 = COORD.format(cx+self.r,cy+self.r, cz+self.r)
-        self.v8 = COORD.format(cx-self.r,cy+self.r, cz+self.r)
-        self.cx = CENTER.format(cx)
-        self.cy = CENTER.format(cy)
-        self.cz = CENTER.format(cz)
-    
-        self.values = [cx, cy, cz, self.v1, self.v2, self.v3, self.v4,
-                       self.v5, self.v6, self.v7, self.v8]
+    cx, cy, cz = center
+    v1 = COORD.format(cx - radius, cy - radius, cz - radius)
+    v2 = COORD.format(cx + radius, cy - radius, cz - radius)
+    v3 = COORD.format(cx + radius, cy - radius, cz + radius)
+    v4 = COORD.format(cx - radius, cy - radius, cz + radius)
+    v5 = COORD.format(cx - radius, cy + radius, cz - radius)
+    v6 = COORD.format(cx + radius, cy + radius, cz - radius)
+    v7 = COORD.format(cx + radius, cy + radius, cz + radius)
+    v8 = COORD.format(cx - radius, cy + radius, cz + radius)
+    cx = CENTER.format(cx)
+    cy = CENTER.format(cy)
+    cz = CENTER.format(cz)
+
+    values = [cx, cy, cz, v1, v2, v3, v4, v5, v6, v7, v8]
+
+    replace = {keyword: value for keyword, value in zip(KEYWORDS, values)}
+
+    tb.TemplateBuilder(file, replace)
+
+    return file
+
 
 if __name__ == "__main__":
-     BoxBuilder([11.910, 59.189, 55.414], 15, "/home/dsoler/box.pdb")
+    center, radius, box_file = parse_args()
+    box = build_box(center, radius, box_file)
+    print("Box created in {}".format(os.path.abspath(box)))
