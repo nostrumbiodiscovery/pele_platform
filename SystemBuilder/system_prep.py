ea8b2665483730e4a174c16e7c24b7a089604519
Daniel Soler <daniel.soler@e-campus.uab.cat>
2018-02-01 00:33:31 +0100

    Including retrieve_receptor and tranforming class to func() for portability

--- SystemBuilder/system_prep.py
+++ SystemBuilder/system_prep.py
@@ -1,68 +1,82 @@
-import sys
 import os
 from schrodinger import structure as st
 
 
-class SystemBuilder(st.StructureReader):
+def build_complexes(ligands, receptor):
+    """
+       Desciption: From each structure retrieve
+       a .mae file, the complex with the receptor
+       and a the ligand's residue name.
 
-    def __init__(self, ligands, receptor):
-        self.ligands = ligands
-        self.receptor = receptor
-        self.structures = st.StructureReader(self.ligands)
+       Output:
+            struct_files_mae: list of ligand.mae files
+            complex: list of receptor+ligand pdb
+            residues: list of residues of ligands
+    """
 
-    def structs(self):
-	"""
-	   Desciption: Split each entry of a mae_file 
-           retriving the residue_naem and the out file.
+    residues = []
+    structures_mae = []
+    structures_pdb = []
 
- 	   Output:
-  		self.struct_files_mae: list of output .mae files
-		residues: list of residues of all lig in  mae files
-	"""
+    # FAIL WHYYY
+    residues.append([residue for residue in structure.residue][0].pdbres.strip())
 
-	self.residues_name = []
-	structures = []
-        structures_mae = []
- 	structures_pdb = []
+    for i, (structure, res) in enumerate(zip(st.StructureReader(ligands), residues)):
 
-        for i, structure in enumerate(self.structures):
-           
-	    self.residues_name.append([residue for residue in structure.residue][0].pdbres.strip())
-       
-	    str_name = "{}".format(structure.title)
-            if str_name in structures:
-                str_name = "ligand_{:d}".format(i)
-	    try:
-	    	structure.write(str_name+".mae")
-		structure.write(str_name+".pdb")
-	    except ValueError:
-                str_name = "ligand_{}".format(i)
-		structure.write(str_name+".mae")
-                structure.write(str_name+".pdb")
-	    finally:
-		structures.append(str_name)
-		structures_mae.append("{}.mae".format(str_name))
-		structures_pdb.append("{}.pdb".format(str_name))
+        str_name = "{}".format(res)
+        if str_name in '\t'.join(structures_mae):
+            str_name = "ligand_{:d}".format(i)
+        try:
+            structure.write(str_name + ".mae")
+            structure.write(str_name + ".pdb")
+        except ValueError:
+            str_name = "ligand_{:d}".format(i)
+        finally:
+            structure.write(str_name + ".mae")
+            structure.write(str_name + ".pdb")
+            structures_mae.append("{}.mae".format(str_name))
+            structures_pdb.append("{}.pdb".format(str_name))
 
-	self.struct_files_mae = [os.path.abspath(structure) for structure in structures_mae]
-	self.struct_files_pdb = [os.path.abspath(structure) for structure in structures_pdb]
+    struct_files_mae = [os.path.abspath(structure) for structure in structures_mae]
+    struct_files_pdb = [os.path.abspath(structure) for structure in structures_pdb]
 
-        return self.struct_files_mae, self.struct_files_pdb ,self.residues_name
+    complexes = systems(receptor, struct_files_pdb)
 
-    def systems(self):
-	"""
-	   Description: From one ligand structure build the 
-	   complex with its receptor.
+    return complexes, struct_files_mae, residues
 
-	   Output:
-		self.struct_files_pdb: List of complexes.
-	"""
-	for structure in self.struct_files_pdb:
-		with open(structure, "r") as pdb:
-		    self.lines = pdb.readlines()
-		    self.clean_lines = [line for line in self.lines if line.startswith("HETATM")]
-		pdb_file = self.receptor + "TER\n" + "".join(self.clean_lines)+"END\n"	
-		with open(structure, "w") as pdb_out:
-		   pdb_out.write(pdb_file)
-	return self.struct_files_pdb			
-       
+
+def systems(receptor, structs):
+    """
+       Description: From one ligand structure build the 
+       complex with its receptor.
+
+       Output:
+            self.struct_files_pdb: List of complexes.
+    """
+    for struct in structs:
+        with open(struct, "r") as pdb:
+            clean_lines = [line for line in pdb.readlines() if line.startswith("HETATM")]
+        system = receptor + "TER\n" + "".join(clean_lines) + "END\n"
+        with open(struct, "w") as pdb_out:
+            pdb_out.write(system)
+    return structs
+
+
+def retrieve_receptor(system):
+    """
+    This function returns receptor of the complex of interest.
+
+    :param complex: system format pdb
+
+    :output: receptor text
+    """
+
+    receptor_text = ""
+
+    with open(system, 'r') as pdb_file:
+        receptor_text = [line for line in pdb_file if line.startswith("ATOM")]
+
+    if receptor_text == "":
+        raise ValueError("Something went wrong when extracting the ligand. Ligand must be a HETATOM")
+
+    return "".join(receptor_text)
