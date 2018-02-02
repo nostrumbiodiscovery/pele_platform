852fb24a3a65703c128baa4acb2ab10f92a42fbe
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-02-02 18:27:55 +0100

    Constants module to cahnge by user

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+
+SCHRODINGER = "/sNow/easybuild/centos/7.4.1708/Skylake/software/schrodinger2017-4/"
+PELE = "/sNow/easybuild/centos/7.4.1708/Skylake/software/PELE/1.5.0.2524/"
+ADAPTIVE = "/home/dsoler/repos/AdaptivePELE/"
+MPIRUN = "/sNow/easybuild/centos/7.4.1708/Skylake/software/OpenMPI/1.8.4-GCC-4.9.2/bin"
+
