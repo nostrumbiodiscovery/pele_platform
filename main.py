0cb0be0dc9f0c5d2100871c635cdb838cb4e2ecc
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-02-08 10:45:27 +0100

    Moving to one system pipeline for simplicity

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ import logging
 import argparse
 import PlopRotTemp.main as plop
 import Helpers.helpers as hp
-import Helpers.prepare_ligand as pl
 import Helpers.pele_env as pele
-import Adaptive.adaptive as ad
-import Adaptive.clusterAdaptiveRun as cl
+import Helpers.simulation as ad
+import Helpers.clusterAdaptiveRun as cl
 import Helpers.center_of_mass as cm
 import Helpers.constraints as ct
-import SystemBuilder.system_prep as sp
-import Box.box as bx
-import ppp.mut_prep4pele as ppp
-import msm.analysis as msm
+import Helpers.system_prep as sp
+import Helpers.box as bx
+import PPP.mut_prep4pele as ppp
+import Helpers.msm_analysis as msm
 
 
 
@@ -93,71 +92,72 @@ def run(system, residue, chain, ligands, forcefield, confile, native, cpus, core
     # Preparative for Pele
     logger.info("Retrieving Ligands & Complexes")
     receptor, lig_ref = sp.retrieve_receptor(system, residue)
-    complexes, ligands, residues = sp.build_complexes(ligands, receptor)
-    pele_dirs = [os.path.abspath("{}_Pele".format(residue)) for residue in residues]
+    lig, residue = sp.build_complexes(lig_ref, receptor)
+    pele_dir = os.path.abspath("{}_Pele".format(residue))
     native = NATIVE.format(os.path.abspath(native), chain) if native else native
-    center_mass = cm.center_of_mass(lig_ref.getvalue()) 
+    center_mass = cm.center_of_mass(lig_ref)
+    print(center_mass) 
 
-    for residue, lig, sys, pele_dir in zip(residues, ligands, complexes, pele_dirs):
- 
-        logger.info("Preparing {} system".format(residue))
-        system_fix, missing_residues, gaps, metals = ppp.main(sys)
-        protein_constraints = ct.retrieve_constraints(system_fix, gaps, metals)
+    logger.info("Preparing {} system".format(residue))
+    system_fix, missing_residues, gaps, metals = ppp.main(system)
+    protein_constraints = ct.retrieve_constraints(system_fix, gaps, metals)
 
-        # Produce Templates of all missing residues
-        logger.info("Running PlopRotTemp")
-        for res, resname, chain in missing_residues:
-            logger.info("Creating template for residue {}".format(res))
-            template, rotamers_file = plop.main(lig, mtor, n, core, mae_charges, clean)
-            hp.silentremove([lig])
-       
+    # Produce Templates of all missing residues
+    logger.info("Running PlopRotTemp")
+    for res, resname, chain in missing_residues:
+        logger.info("Creating template for residue {}".format(res))
+        template, rotamers_file = plop.main(lig, mtor, n, core, mae_charges, clean)
+        hp.silentremove([lig])
    
-        logger.info("Creating Pele env")
-        adap_ex_input = os.path.join(pele_dir, system_fix)
-        adap_ex_output = os.path.join(pele_dir, "output_adaptive_exit")
-        cluster_output = os.path.join(pele_dir, "output_clustering")
-        adap_l_input = "{}/initial_*"
-        adap_l_output = os.path.join(pele_dir, "output_adaptive_long")
-        ad_ex_temp = os.path.join(pele_dir, "adaptive_exit.conf")
-        ad_l_temp = os.path.join(pele_dir, "adaptive_long.conf")
-        pele_temp = os.path.join(pele_dir, "pele.conf")
-        box_temp = os.path.join(pele_dir, "box.pdb")
-        clusters = os.path.join(cluster_output, "clusters_40_KMeans_allSnapshots.pdb")
-        
-        files = [os.path.basename(system_fix), os.path.join(DIR, "PeleTemplates/box.pdb"), os.path.join(DIR, "PeleTemplates/pele.conf"),
-                 os.path.join(DIR, "PeleTemplates/adaptive_exit.conf"), os.path.join(DIR, "PeleTemplates/adaptive_long.conf")]
-        directories = FOLDERS
-        directories.extend(["output_adaptive_long", "output_adaptive_exit", "output_clustering"])
-        pele.set_pele_env(system_fix, directories, files, forcefield, template, rotamers_file, pele_dir)
 
-        
-        logger.info("Preparing ExitPath Adaptive Env")
-        ad.AdaptiveBuilder(pele_temp, EX_PELE_KEYWORDS, native, forcefield, chain, "\n".join(protein_constraints))
-        adaptive_exit = ad.AdaptiveBuilder(ad_ex_temp, ADAPTIVE_KEYWORDS, RESTART, adap_ex_output, adap_ex_input, cpus, pele_temp, residue)
-        adaptive_exit.run()  
-       
+    logger.info("Creating Pele env")
+    adap_ex_input = os.path.join(pele_dir, os.path.basename(system_fix))
+    adap_ex_output = os.path.join(pele_dir, "output_adaptive_exit")
+    cluster_output = os.path.join(pele_dir, "output_clustering")
+    adap_l_input = "{}/initial_*"
+    adap_l_output = os.path.join(pele_dir, "output_pele")
+    ad_ex_temp = os.path.join(pele_dir, "adaptive_exit.conf")
+    ad_l_temp = os.path.join(pele_dir, "adaptive_long.conf")
+    pele_temp = os.path.join(pele_dir, "pele.conf")
+    box_temp = os.path.join(pele_dir, "box.pdb")
+    clusters = os.path.join(cluster_output, "clusters_40_KMeans_allSnapshots.pdb")
+    
+    files = [os.path.join(DIR, "Templates/box.pdb"), os.path.join(DIR, "Templates/pele.conf"),
+             os.path.join(DIR, "Templates/adaptive_exit.conf"), os.path.join(DIR, "Templates/adaptive_long.conf")]
+    directories = FOLDERS
+    directories.extend(["output_pele", "output_adaptive_exit", "output_clustering"])
+    pele.set_pele_env(system_fix, directories, files, forcefield, template, rotamers_file, pele_dir)
 
-        logger.info("MSM Clustering")
-        with pele.cd(adap_ex_output):
-            cl.main(num_clusters=CLUSTERS, output_folder=cluster_output, ligand_resname=residue, atom_ids="")
+    
+    logger.info("Preparing ExitPath Adaptive Env")
+    ad.SimulationBuilder(pele_temp, EX_PELE_KEYWORDS, native, forcefield, chain, "\n".join(protein_constraints))
+    adaptive_exit = ad.SimulationBuilder(ad_ex_temp, ADAPTIVE_KEYWORDS, RESTART, adap_ex_output, adap_ex_input, cpus, pele_temp, residue)
+    adaptive_exit.run()  
+   
 
-        logger.info("Create box")
-        center, radius = bx.main(adap_ex_output , clusters , center_mass)
-        box = bx.build_box(center, radius, box_temp)
+    logger.info("MSM Clustering")
+    with hp.cd(adap_ex_output):
+        cl.main(num_clusters=CLUSTERS, output_folder=cluster_output, ligand_resname=residue, atom_ids="")
 
-        logger.info("Running standard Pele")
-        ad.AdaptiveBuilder(pele_temp, PELE_KEYWORDS, center, radius)
-        adaptive_long = ad.AdaptiveBuilder(ad_l_temp, ADAPTIVE_KEYWORDS, RESTART, adap_l_output, adap_l_input, cpus, pele_temp, residue)
-        adaptive_long.run()
-       
-        logger.info("Extracting dG with MSM analysis") 
-        msm.analyse_results(adap_l_output, residue)
+    logger.info("Create box")
+    center, radius = bx.main(adap_ex_output , clusters , center_mass)
+    box = bx.build_box(center, radius, box_temp)
 
-    # Analyze results
-    output = msm.summerize(pele_dirs, residues)
-    output.insert(0, OUTPUT_HEADER)
-    with open(RANKING_FILE, "w") as fout:
-        fout.write("".join(output))
+    logger.info("Running standard Pele")
+    ad.SimulationBuilder(pele_temp, PELE_KEYWORDS, center, radius)
+    adaptive_long = ad.SimulationBuilder(ad_l_temp, ADAPTIVE_KEYWORDS, RESTART, adap_l_output, adap_l_input, cpus, pele_temp, residue)
+    adaptive_long.run()
+   
+    logger.info("Extracting dG with MSM analysis") 
+    msm.analyse_results(adap_l_output, residue)
+    
+    logger.info("{} System run successfully".format(residue))
+
+    # # Analyze results
+    # output = msm.summerize(pele_dirs, residues)
+    # output.insert(0, OUTPUT_HEADER)
+    # with open(RANKING_FILE, "w") as fout:
+    #     fout.write("".join(output))
 
 if __name__ == "__main__":
 
