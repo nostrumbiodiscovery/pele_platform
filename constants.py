732ed92509cf2d92ca963603d6aeb540d3a0011c
jgilaber <joan.gilabert@bsc.es>
2018-02-09 13:18:07 +0100

    Update constants for MN

--- constants.py
+++ constants.py
@@ -3,9 +3,9 @@ machine = socket.getfqdn()
 
 if "bsc.mn" in machine:
     SCHRODINGER = "/gpfs/projects/bsc72/SCHRODINGER_ACADEMIC"
-    PELE = "/gpfs/projects/bsc72/PELE++/mniv/rev12455/bin/PELE-1.5_mpi"
+    PELE = "/gpfs/projects/bsc72/PELE++/mniv/rev12455"
     ADAPTIVE = "/gpfs/projects/bsc72/adaptiveSampling/bin/v1.4.1"
-    MPIRUN = "/apps/INTEL/2017.4/impi/2017.3.196/bin64/mpirun"
+    MPIRUN = "/apps/INTEL/2017.4/impi/2017.3.196/bin64"
 else:
     SCHRODINGER = "/sNow/easybuild/centos/7.4.1708/Skylake/software/schrodinger2017-4/"
     PELE = "/sNow/easybuild/centos/7.4.1708/Skylake/software/PELE/1.5.0.2524/"
