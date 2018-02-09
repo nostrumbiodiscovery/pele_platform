e06335b71f4d6c7a662ce8666e3f92083ea95405
jgilaber <joan.gilabert@bsc.es>
2018-02-09 13:20:23 +0100

    Remove useless lines in PPP/enviroment_parameters

--- PPP/enviroment_parameters.py
+++ PPP/enviroment_parameters.py
@@ -1,8 +1,6 @@
 import sys
-sys.path.append("/home/dsoler/PelePlop/")
-__author__ = 'jelisa'
 from MSM_PELE import constants 
 
-#pele_folder_path = "/sNow/easybuild/centos/7.4.1708/Skylake/software/PELE/1.5.0.2524/"
-pele_folder_path = constants.PELE
+__author__ = 'jelisa'
 
+pele_folder_path = constants.PELE
