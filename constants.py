eaf014a7e1685c861943023284639cf44e6ee3ae
jgilaber <joan.gilabert@bsc.es>
2018-02-21 11:49:28 +0100

    Add block for nord3 in constants.py

--- constants.py
+++ constants.py
@@ -8,6 +8,13 @@ if "bsc.mn" in machine:
     MPIRUN = "/apps/INTEL/2017.4/impi/2017.3.196/bin64"
     LICENSE = "/gpfs/projects/bsc72/PELE++/license"
 
+elif "mn.bsc":
+    SCHRODINGER = "/gpfs/projects/bsc72/SCHRODINGER_ACADEMIC_MN3"
+    PELE = "/gpfs/projects/bsc72/PELE++/nord/rev12489"
+    ADAPTIVE = "/gpfs/projects/bsc72/adaptiveSampling/bin_nord/v1.4.1"
+    MPIRUN = "/apps/OPENMPI/1.8.1-mellanox/bin"
+    LICENSE = "/gpfs/projects/bsc72/PELE++/license"
+
 else:
     SCHRODINGER = "/sNow/easybuild/centos/7.4.1708/Skylake/software/schrodinger2017-4/"
     PELE = "/sNow/easybuild/centos/7.4.1708/Skylake/software/PELE/1.5.0.2524/"
