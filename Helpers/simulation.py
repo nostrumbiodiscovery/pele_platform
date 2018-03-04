86acbd0ec6c28009881c834a2066d7fd9a3d0bdc
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2018-03-04 17:15:15 +0000

    Index array change due to adaptive modification

--- Helpers/simulation.py
+++ Helpers/simulation.py
@@ -26,13 +26,13 @@ class SimulationBuilder(template_builder.TemplateBuilder):
             else:
 				ad.main(self.file)
 
-    def interactive_clustering(self, cluster_object, paths, simulationRunner, i):
+    def interactive_clustering(self, cluster_object, paths, simulationRunner, epoch_number):
         initial_rmsd_cluster_values = cluster_object.thresholdCalculator.values
         while len(cluster_object.clusters.clusters) == 1:
             current_values = cluster_object.thresholdCalculator.values
             cluster_object.thresholdCalculator.values = [ value-0.5 if value > 0.5 else value for value in current_values]
             cluster_object.emptyClustering()
-            clusterPreviousEpochs(cluster_object, i+1, paths.epochOutputPathTempletized, simulationRunner)
+            clusterPreviousEpochs(cluster_object, epoch_number, paths.epochOutputPathTempletized, simulationRunner)
             print("Lowering cluster RMSD to: {}".format(cluster_object.thresholdCalculator.values))
         cluster_object.thresholdCalculator.values = initial_rmsd_cluster_values
         print("Interactive clustering ended restoring initial value {}".format(cluster_object.thresholdCalculator.values))
