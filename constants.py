c430b335e185d32d5b6fe3ac8fc77577e40f4a9d
jgilaber <joan.gilabert@bsc.es>
2018-03-23 15:34:54 +0100

    Minor changes in life cluser constants

--- constants.py
+++ constants.py
@@ -25,11 +25,13 @@ elif "mn.bsc" in machine:
 elif "bsccv" in machine:
     SCHRODINGER = "/data2/bsc72/SCHRODINGER_ACADEMIC"
     PELE = "/data/EAPM/PELE/PELE++/life/rev12489"
-    ADAPTIVE = "/data2/bsc72/AdaptiveSampling/bin_nord/v1.4.2"
+    ADAPTIVE = "/data2/bsc72/AdaptiveSampling/bin/v1.4.2_bis"
     MPIRUN = "/data2/apps/OPENMPI/1.6.1/bin"
+    #LICENSE = "/gpfs/projects/bsc72/PELE++/license"
+    MMSHARE = None
     LICENSE = "/data/EAPM/PELE/PELE++/license"
-    MMSHARE = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.4/bin/Linux-x86_64"
-    SCHRODINGER_PYTHON_LIBS = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.4/lib/Linux-x86_64/lib/python2.7/site-packages/"
+    # MMSHARE = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.9/bin/Linux-x86_64"
+    # SCHRODINGER_PYTHON_LIBS = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.9/lib/Linux-x86_64/lib/python2.7/site-packages/"
 else:
     SCHRODINGER = "/sNow/easybuild/centos/7.4.1708/Skylake/software/schrodinger2017-4/"
     PELE = "/sNow/easybuild/centos/7.4.1708/Skylake/software/PELE/1.5.0.2524/"
