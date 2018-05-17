1dba67b1e950b447d4ab7a8433b4c4fbc430c0b7
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-05-17 17:29:33 +0200

    Change test metrics when new metric is added

--- Helpers/simulation.py
+++ Helpers/simulation.py
@@ -32,7 +32,7 @@ class SimulationBuilder(template_builder.TemplateBuilder):
             cluster_object.thresholdCalculator.values = [ value-0.5 if value > 0.5 else value for value in current_values]
             if cluster_object.thresholdCalculator.values == current_values: break
             cluster_object.emptyClustering()
-            ad.clusterPreviousEpochs(cluster_object, epoch_number, paths.epochOutputPathTempletized, simulationRunner)
+            ad.clusterPreviousEpochs(cluster_object, epoch_number, paths.epochOutputPathTempletized, simulationRunner, None)
             print("Lowering cluster RMSD to: {}".format(cluster_object.thresholdCalculator.values))
         cluster_object.thresholdCalculator.values = initial_rmsd_cluster_values
         print("Interactive clustering ended restoring initial value {}".format(cluster_object.thresholdCalculator.values))
