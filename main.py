fe4348fa8fc564a47032982dd8299eed1e690c8a
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-07-03 16:36:16 +0200

    Migrate to PELE modules version where a launcher controls wich task you want to permorf (msm/glide at the moment) but it's build to easily implement other tasks

--- main.py
+++ main.py
@@ -1,99 +1,36 @@
-import os
-import MSM_PELE.Helpers.check_env_var as env
-env.check_dependencies()
-import shutil
 import argparse
+import MSM_PELE.msm as msm
 import MSM_PELE.constants as cs
-import MSM_PELE.PlopRotTemp.main as plop
-import MSM_PELE.Helpers.helpers as hp
-import MSM_PELE.Helpers.pele_env as pele
-import MSM_PELE.Helpers.simulation as ad
-import MSM_PELE.Helpers.clusterAdaptiveRun as cl
-import MSM_PELE.Helpers.center_of_mass as cm
-import MSM_PELE.Helpers.system_prep as sp
-import MSM_PELE.Helpers.box as bx
-import MSM_PELE.PPP.mut_prep4pele as ppp
-import MSM_PELE.Helpers.msm_analysis as msm
-import MSM_PELE.Helpers.missing_residues as mr
-
-__version__ = "1.0.3"
-
-def run(args):
-    # Build folders and logging
-    env = pele.EnviroBuilder.build_env(args)
-
-    if args.restart == "all":
-
-        # Build System
-        env.logger.info("Checking {} system for Pele".format(args.residue))
-        syst = sp.SystemBuilder.build_system(args.system, args.mae_lig, args.residue, env.pele_dir)
-
-        # Prepare System
-        system_fix, missing_residues, gaps, metals, protein_constraints = ppp.main(syst.system, env.pele_dir, charge_terminals=args.charge_ter, no_gaps_ter=args.gaps_ter)
-        env.logger.info(cs.SYSTEM.format(system_fix, missing_residues, gaps, metals))
-
-        # Parametrize Ligand
-        env.logger.info("Creating template for residue {}".format(args.residue))
-        plop.parametrize_miss_residues(args, env, syst)
-        env.logger.info("Template {}z created".format(args.residue.lower()))
-
-        # Parametrize missing residues
-        for res, __, _ in missing_residues:
-            if res != args.residue:
-                env.logger.info("Creating template for residue {}".format(res))
-                mr.create_template(system_fix, res, env.pele_dir, args.forcefield)
-                env.logger.info("Template {}z created".format(res))
-
-        # Fill in Simulation Templates
-        ad.SimulationBuilder(env.pele_exit_temp,  env.topology, cs.EX_PELE_KEYWORDS, env.native, args.forcefield, args.chain, "\n".join(protein_constraints), args.cpus, env.license)
-        ad.SimulationBuilder(env.pele_temp,  env.topology, cs.EX_PELE_KEYWORDS, env.native, args.forcefield, args.chain, "\n".join(protein_constraints), args.cpus, env.license)
-
-    if args.restart in ["all", "adaptive"]:
-        # Run Adaptive Exit
-        env.logger.info("Running ExitPath Adaptive")
-        adaptive_exit = ad.SimulationBuilder(env.ad_ex_temp, env.topology, cs.EX_ADAPTIVE_KEYWORDS, cs.RESTART, env.adap_ex_output,
-            env.adap_ex_input, env.cpus, env.pele_exit_temp, env.residue, env.equil_steps, env.random_num)
-        adaptive_exit.run(hook=True)
-        env.logger.info("ExitPath Adaptive run successfully")
+import MSM_PELE.glide as gl
 
 
-    if args.restart in ["all", "adaptive", "pele"]:
+class Launcher():
 
-        #KMeans Clustering
-        if not os.path.isfile(env.clusters_output):
-            env.logger.info("Running MSM Clustering")
-            with hp.cd(env.adap_ex_output):
-                cl.main(env.clusters, env.cluster_output, args.residue, "", env.cpus, env.topology)
-            env.logger.info("MSM Clustering run successfully")
+
+    def __init__(self, args):
+        self.args = args
+
+    def launch(self):
+        if not args.hbond:
+            if(args.clust > args.cpus and args.restart != "msm" and not args.test):
+                raise ValueError(cs.CLUSTER_ERROR.format(args.cpus, args.clust))
+            else:
+                msm.run(args)
+
         else:
-            pass
+            gl.run(args)
 
-        # Create Box
-        env.logger.info("Creating box")
-        center, radius, BS_sasa_min, BS_sasa_max = bx.create_box(args, env)
-        env.logger.info("Box with center {} radius {} was created".format(center, radius))
-
-        # Pele Exploration
-        env.logger.info("Running standard Pele")
-        ad.SimulationBuilder(env.pele_temp,  env.topology, cs.PELE_KEYWORDS, center, radius, BS_sasa_min, BS_sasa_max)
-        adaptive_long = ad.SimulationBuilder(env.ad_l_temp,  env.topology, cs.ADAPTIVE_KEYWORDS,
-            cs.RESTART, env.adap_l_output, env.adap_l_input, args.cpus, env.pele_temp, args.residue, env.random_num)
-        adaptive_long.run()
-        env.logger.info("Pele run successfully")
-
-    if args.restart in ["all", "adaptive", "pele", "msm"]:
-
-        # MSM Analysis
-        env.logger.info("Running MSM analysis")
-        msm.analyse_results(env, args, runTica=False)
-        env.logger.info("MSM analysis run successfully")
-
-        env.logger.info("{} System run successfully".format(args.residue))
+    @property
+    def args(self):
+        return self.args
 
 
-if __name__ == "__main__":
+class ArgsHandler:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-    parser = argparse.ArgumentParser(description='Run Adaptive Pele Platform')
+def parseargs():
+    parser = argparse.ArgumentParser(description='Run PELE Platform')
     parser.add_argument('system', type=str, help='complex to run pele on')
     parser.add_argument('residue', type=str, help='residue of the ligand to extract', default=cs.LIG_RES)
     parser.add_argument('chain', type=str, help='chain of the ligand to extract', default=cs.LIG_CHAIN)
@@ -118,9 +55,13 @@ if __name__ == "__main__":
     parser.add_argument("--user_radius", "-r", type=float,  help="Radius of the box", default=None)
     parser.add_argument("--folder", "-wf", type=str,  help="Folder to apply the restart to", default=None)
     parser.add_argument("--pdb", action='store_true',  help="Use pdb files as output")
-    
-    args = parser.parse_args()
-    if(args.clust > args.cpus and args.restart != "msm" and not args.test ):
-        raise ValueError(cs.CLUSTER_ERROR.format(args.cpus, args.clust))
-    else:
-        run(args)
+    parser.add_argument("--hbond", nargs='+',  help="Definition of kinase hbond", default=None)
+    parser.add_argument("--precision_glide", type=str,  help="Glide precision.. Options = [SP, XP]", default="SP")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parseargs()
+    print(args.hbond)
+    platform_object = Launcher(args)
+    platform_object.launch()
