230392e32b8375c05f021e0565b69febc1c3e7c4
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-07-03 15:05:50 +0200

    Add handler to choose between msm/glide environment preparation

--- Helpers/pele_env.py
+++ Helpers/pele_env.py
@@ -12,9 +12,10 @@ class EnviroBuilder(object):
         is build by creating folders and files
     """
 
-    def __init__(self, folders, files, args):
+    def __init__(self, folders, files, file_names, args):
         self.folders = folders
         self.files = files
+        self.file_names = file_names
         self.system = args.system
         self.forcefield = args.forcefield
         self.residue = args.residue
@@ -28,19 +29,32 @@ class EnviroBuilder(object):
         self.test = args.test
         self.folder = args.folder
         self.pdb = args.pdb
+        self.hbond = args.hbond
+        self.precision_glide = args.precision_glide
         self.build_constant_paths()
 
     @classmethod
     def build_env(cls, args):
-        if args.test:
-            env = cls(cs.FOLDERS, cs.FILES_TEST, args)
-        elif args.precision:
-            env = cls(cs.FOLDERS, cs.FILES_XP, args)
-        else:
-            env = cls(cs.FOLDERS, cs.FILES_SP, args)
+        FOLDERS, FILES, FILE_NAMES = cls.choose_env(args)
+        env = cls(FOLDERS, FILES, FILE_NAMES, args)
         env.create()
         return env
 
+    @staticmethod
+    def choose_env(args):
+        if args.test and args.restart != "glide":
+            return  cs.FOLDERS, cs.FILES_TEST, cs.FILES_NAME_MSM
+        elif args.test and args.restart == "glide":
+            return cs.FOLDERS_GLIDE, cs.FILES_GLIDE_TEST, cs.FILES_NAME_GLIDE
+        elif args.restart == "glide":
+            return cs.FOLDERS_GLIDE, cs.FILES_GLIDE, cs.FILES_NAME_GLIDE
+        elif args.precision:
+            return  cs.FOLDERS, cs.FILES_XP, cs.FILES_NAME_MSM
+        else:
+            return cs.FOLDERS, cs.FILES_SP, cs.FILES_NAME_MSM
+            
+            
+            
     def build_constant_paths(self):
 
         self.template = None
@@ -56,7 +70,7 @@ class EnviroBuilder(object):
         pele_dir = os.path.abspath("{}_Pele".format(self.residue))
 
         if not self.folder:
-            self.pele_dir = is_repited(pele_dir) if self.restart == "all" else is_last(pele_dir)
+            self.pele_dir = is_repited(pele_dir) if self.restart in cs.FIRST_RESTART else is_last(pele_dir)
         else:
             self.pele_dir = os.path.abspath(self.folder)
 
@@ -65,23 +79,39 @@ class EnviroBuilder(object):
         else:
             self.system_fix = os.path.join(self.pele_dir, "{}_processed.pdb".format(os.path.abspath(os.path.splitext(self.system)[0])))
 
-        self.adap_ex_input = os.path.join(self.pele_dir, os.path.basename(self.system_fix))
-        self.adap_ex_output = os.path.join(self.pele_dir, "output_adaptive_exit")
-        self.cluster_output = os.path.join(self.pele_dir, "output_clustering")
-        self.adap_l_input = "{}/initial_*"
-        self.adap_l_output = os.path.join(self.pele_dir, "output_pele")
-        self.ad_ex_temp = os.path.join(self.pele_dir, "adaptive_exit.conf")
-        self.ad_l_temp = os.path.join(self.pele_dir, "adaptive_long.conf")
-        self.pele_exit_temp = os.path.join(self.pele_dir, "pele_exit.conf")
-        self.pele_temp = os.path.join(self.pele_dir, "pele.conf")
-        self.box_temp = os.path.join(self.pele_dir, "box.pdb")
-        self.clusters_output = os.path.join(self.cluster_output, "clusters_{}_KMeans_allSnapshots.pdb".format(self.clusters))
-        self.ligand_ref = os.path.join(self.pele_dir, "ligand.pdb")
-        self.native = cs.NATIVE.format(os.path.abspath(self.native), self.chain) if self.native else cs.NATIVE.format(os.path.abspath(self.ligand_ref), self.chain)
-        self.topology = None if self.pdb else os.path.join(self.adap_ex_output, "topology.pdb")
+
+        if not self.hbond:
+            self.adap_ex_input = os.path.join(self.pele_dir, os.path.basename(self.system_fix))
+            self.adap_ex_output = os.path.join(self.pele_dir, "output_adaptive_exit")
+            self.cluster_output = os.path.join(self.pele_dir, "output_clustering")
+            self.adap_l_input = "{}/initial_*"
+            self.adap_l_output = os.path.join(self.pele_dir, "output_pele")
+            self.ad_ex_temp = os.path.join(self.pele_dir, "adaptive_exit.conf")
+            self.ad_l_temp = os.path.join(self.pele_dir, "adaptive_long.conf")
+            self.pele_exit_temp = os.path.join(self.pele_dir, "pele_exit.conf")
+            self.pele_temp = os.path.join(self.pele_dir, "pele.conf")
+            self.box_temp = os.path.join(self.pele_dir, "box.pdb")
+            self.clusters_output = os.path.join(self.cluster_output, "clusters_{}_KMeans_allSnapshots.pdb".format(self.clusters))
+            self.ligand_ref = os.path.join(self.pele_dir, "ligand.pdb")
+            self.native = cs.NATIVE.format(os.path.abspath(self.native), self.chain) if self.native else cs.NATIVE.format(os.path.abspath(self.ligand_ref), self.chain)
+            self.topology = None if self.pdb else os.path.join(self.adap_ex_output, "topology.pdb")
+
+        if self.hbond:
+             self.adap_ex_input = os.path.join(self.pele_dir, os.path.basename(self.system_fix))
+             self.adap_ex_output = os.path.join(self.pele_dir, "output_adaptive")
+             self.cluster_output = os.path.join(self.pele_dir, "output_clustering")
+             self.glide_template = os.path.join(self.pele_dir, "glide.in") 
+             self.ad_ex_temp = os.path.join(self.pele_dir, "adaptive.conf")
+             self.pele_exit_temp = os.path.join(self.pele_dir, "pele.conf")
+             self.clusters_output = os.path.join(self.cluster_output, "clusters_{}_KMeans_allSnapshots.pdb".format(self.clusters))
+             self.pele_temp = os.path.join(self.pele_dir, "pele.conf")
+             self.ligand_ref = os.path.join(self.pele_dir, "ligand.pdb")
+             self.native = cs.NATIVE.format(os.path.abspath(self.native), self.chain) if self.native else cs.NATIVE.format(os.path.abspath(self.ligand_ref), self.chain)
+             self.glide_structs = os.path.join(self.pele_dir, "glide_calculations", "structures")
+             self.topology = None if self.pdb else os.path.join(self.adap_ex_output, "topology.pdb")
 
     def create(self):
-        if self.restart == "all":
+        if self.restart in cs.FIRST_RESTART:
             self.create_folders()
             self.create_files()
             self.create_logger()
@@ -102,8 +132,9 @@ class EnviroBuilder(object):
         """
 
         # Actions
-        for file, destination_name in zip(self.files, cs.FILES_NAME):
+        for file, destination_name in zip(self.files, self.file_names):
             self.copy(file, os.path.join(self.pele_dir, destination_name))
+
 
     def create_dir(self, base_dir, extension=None):
         """
@@ -139,7 +170,7 @@ class EnviroBuilder(object):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
         formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
-        if self.restart == "all":
+        if self.restart in ["all", "glide" ]:
 			file_handler = logging.FileHandler(log_name, mode='w')
         else:
 			file_handler = logging.FileHandler(log_name, mode='a')
