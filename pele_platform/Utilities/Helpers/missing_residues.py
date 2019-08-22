78ec6893d19f690015d135bed08bb03b975068da
dani <daniel.soler@e-campus.uab.cat>
2019-08-22 05:24:44 +0200

    Fix bug on when parametrizing missing residues

--- pele_platform/Utilities/Helpers/missing_residues.py
+++ pele_platform/Utilities/Helpers/missing_residues.py
@@ -4,9 +4,9 @@ import pele_platform.Utilities.PlopRotTemp.launcher as plop
 import pele_platform.Utilities.Helpers.system_prep as sp
 import pele_platform.Utilities.Helpers.helpers as hp
 
-def create_template(args, env):
+def create_template(args, env, residue):
     template_dir = os.path.join(env.pele_dir, "DataLocal/Templates/{}/HeteroAtoms/".format(env.forcefield))
     rotamers_dir = os.path.join(env.pele_dir, "DataLocal/LigandRotamerLibs")
     output_pdb = os.path.join(env.pele_dir, "miss_residue.pdb")
-    syst = sp.SystemBuilder.build_system(env.system_fix, None, env.residue, env.pele_dir, output=output_pdb)
-    plop.parametrize_miss_residues(args, env, syst)
+    syst = sp.SystemBuilder.build_system(env.system_fix, None, residue, env.pele_dir, output=output_pdb)
+    plop.parametrize_miss_residues(args, env, syst, residue)
