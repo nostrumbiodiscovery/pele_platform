d71c3afe3e3e992a621082a89f0898675f3ab45f
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-11-22 16:22:49 +0100

    Update to version 1.2

--- pele_platform/Utilities/Helpers/deploy_testpypi.sh
+++ pele_platform/Utilities/Helpers/deploy_testpypi.sh
@@ -1,8 +1,12 @@
 #Clean and reinstall requirements
 pip uninstall -r requirements.txt --yes
+pip uninstall mdtraj --yes
 pip uninstall pele_platform --yes
 pip install cython numpy
 pip install .
+
+#fast test
+python -m pele_platform.main -h
 
 #Clean and build
 rm -r dist pele_platform.egg*
--- setup.py
+++ setup.py
@@ -1,37 +1,9 @@
-import numpy
 import pele_platform
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
 from distutils.extension import Extension
-try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-try:
-    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
-    # for pip <= 9.0.3
-    from pip.req import parse_requirements
-from distutils.command.sdist import sdist as _sdist
-
-# Run the following line to compile atomset package
-# python setup.py build_ext --inplace
-
-
-class sdist(_sdist):
-    def run(self):
-        # Make sure the compiled Cython files in the distribution are
-        # up-to-date
-        from Cython.Build import cythonize
-        cythonize(['cython/mycythonmodule.pyx'])
-        _sdist.run(self)
-        cmdclass['sdist'] = sdist
 
 here = path.abspath(path.dirname(__file__))
 ext_modules = []
@@ -39,23 +11,6 @@ cmdclass = {}
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
      long_description = f.read()
-
-if use_cython:
-    ext_modules += [
-            Extension("pele_platform.AdaptivePELE.atomset.atomset", ["pele_platform/AdaptivePELE/atomset/atomset.pyx"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.atomset.SymmetryContactMapEvaluator", ["pele_platform/AdaptivePELE/atomset/SymmetryContactMapEvaluator.pyx"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.atomset.RMSDCalculator", ["pele_platform/AdaptivePELE/atomset/RMSDCalculator.pyx"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.freeEnergies.utils", ["pele_platform/AdaptivePELE/freeEnergies/utils.pyx"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/freeEnergies"])
-    ]
-    cmdclass.update({'build_ext': build_ext})
-else:
-    ext_modules += [
-        Extension("pele_platform.AdaptivePELE.atomset.atomset", ["pele_platform/AdaptivePELE/atomset/atomset.c"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.atomset.SymmetryContactMapEvaluator", ["pele_platform/AdaptivePELE/atomset/SymmetryContactMapEvaluator.c"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/Utilities/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.atomset.RMSDCalculator", ["pele_platform/AdaptivePELE/atomset/RMSDCalculator.c"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/atomset"]),
-        Extension("pele_platform.AdaptivePELE.freeEnergies.utils", ["pele_platform/AdaptivePELE/freeEnergies/utils.c"], include_dirs=["pele_platform/AdaptivePELE", "pele_platform/AdaptivePELE/freeEnergies"])
-    ]
-
 
 setup(
     name="pele_platform",
@@ -68,9 +23,8 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
     package_data={"pele_platform/AdaptivePELE/atomset": ['*.pxd'], "pele_platform/AdaptivePELE/freeEnergies/": ['*.pyx']},
     include_package_data=True,
-    include_dirs=[numpy.get_include()],
-    install_requires=["pillow", "scipy", "matplotlib", 
-       "biopython", "pandas", "pyemma", "prody", "six", "future", "fpdf", "pytest"],    
+    install_requires=["AdaptivePELE", "pillow", "scipy", "matplotlib", 
+       "biopython", "pandas", "pyemma", "prody", "six", "future", "fpdf", "pytest", "cython", "numpy"],    
     cmdclass=cmdclass,
-    ext_modules=cythonize(ext_modules)  # accepts a glob pattern
+    ext_modules=ext_modules  # accepts a glob pattern
 )
