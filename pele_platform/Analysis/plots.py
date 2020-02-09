39341a188025b21888ee20d07f21ef9b6354454c
Dani Soler <daniel.soler@e-campus.uab.cat>
2020-02-09 13:31:11 +0100

    include ligand clustering

--- pele_platform/Analysis/plots.py
+++ pele_platform/Analysis/plots.py
@@ -1,5 +1,9 @@
 import os
+import mdtraj
+import numpy as np
 import pandas as pd
+import hdbscan
+from multiprocessing import Pool
 import matplotlib.pyplot as plt
 import AdaptivePELE.analysis.selectOnPlot as sp
 import AdaptivePELE.analysis.bestStructs as bs
@@ -8,16 +12,31 @@ EPOCH = "epoch"
 STEPS = 3
 TRAJECTORY = "trajectory"
 
-
+def _extract_coords(info):
+    p, v, resname = info
+    # Most time consuming step 0.1
+    traj = mdtraj.load_frame(p, v, top="topology.pdb")
+    atoms_info  = traj.topology.to_dataframe()[0]
+    condition = atoms_info['resName'] == resname
+    atom_numbers_ligand = atoms_info[condition].serial.tolist()
+    coords = []
+    for atom_num in atom_numbers_ligand:
+        try:
+            coords.extend(traj.xyz[0, atom_num-1].tolist())
+        except IndexError:
+            continue
+    return coords
 
 class PostProcessor():
 
-    def __init__(self, report_name, traj_name, simulation_path, topology=False):
+    def __init__(self, report_name, traj_name, simulation_path, cpus, topology=False, residue=False):
         self.report_name = report_name
         self.traj_name = traj_name
         self.simulation_path = simulation_path
         self.data = self.retrive_data()
-        self.topology = False
+        self.topology = topology
+        self.residue = residue
+        self.cpus = cpus
 
     def retrive_data(self, separator=","):
         summary_csv_filename = os.path.join(self.simulation_path, "summary.csv")
@@ -27,7 +46,9 @@ class PostProcessor():
         dataframe = pd.read_csv(summary_csv_filename, sep=separator, engine='python', header=0)
         return dataframe
 
-    def plot_two_metrics(self, column_to_x, column_to_y, column_to_z=False, output_name=None):
+    def plot_two_metrics(self, column_to_x, column_to_y, column_to_z=False, output_name=None, output_folder="."):
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
 
         column_to_x = column_to_x if not str(column_to_x).isdigit() else self._get_column_name(self.data, column_to_x)
         column_to_y = column_to_y if not str(column_to_y).isdigit() else self._get_column_name(self.data, column_to_y)
@@ -42,13 +63,13 @@ class PostProcessor():
             cbar.ax.set_ylabel(column_to_z)
             ax.set_xlabel(column_to_x)
             ax.set_ylabel(column_to_y)
-            plt.savefig(output_name)
+            plt.savefig(os.path.join(output_folder, output_name))
             print("Plotted {} vs {} vs {}".format(column_to_x, column_to_y, column_to_z))
         else:
             output_name = output_name if output_name else "{}_{}_plot.png".format(column_to_x, column_to_y)
             output_name = output_name.replace(" ", "_")
             pts = ax.scatter(self.data[column_to_x], self.data[column_to_y], s=20)
-            plt.savefig(output_name)
+            plt.savefig(os.path.join(output_folder, output_name))
             print("Plotted {} vs {}".format(column_to_x, column_to_y))
 
     def top_poses(self, metric, n_structs, output="BestStructs"):
@@ -75,29 +96,89 @@ class PostProcessor():
             else:
                 bs.extract_snapshot_from_xtc(path, f_id, output, self.topology, step, out_freq, f_out)
 
+    def cluster_poses(self, n_structs, metric, output):
+        assert self.residue, "Set residue ligand name to clusterize"
+        metric = metric if not str(metric).isdigit() else self._get_column_name(self.data, metric)
+        best_poses = self.data.nsmallest(n_structs, metric)
+        self._cluster(best_poses, metric, output)
+
+    def _cluster(self, poses, metric, output):
+        # Extract metric values
+        values = poses[metric].tolist()
+        epochs = poses[EPOCH].tolist()
+        paths = poses[TRAJECTORY].tolist()
+        file_ids = [traj[:-4].split("_")[-1] for traj in paths]
+        steps = self._get_column_name(self.data, STEPS)
+        snapshots = poses[steps].tolist()
+        # Extract coords 
+        all_coords = []
+        pool = Pool(processes=self.cpus)
+        input_pool = [[p,v,self.residue] for p, v in zip(paths, snapshots)]
+        all_coords = pool.map(_extract_coords, input_pool)
+        # Clusterize
+        try:
+            db = hdbscan.HDBSCAN(min_samples=10).fit(all_coords)
+        except ValueError:
+            raise ValueError("Ligand not found check the option --resname. i.e python interactive.py 5 6 7 --resname LIG")
+        indexes = db.labels_
+        n_clusters = len(set(indexes))
+        files_out = ["cluster{}_epoch{}_trajectory_{}.{}_{}{}.pdb".format(cluster, epoch, report, int(step), metric.replace(" ",""), value) \
+           for epoch, step, report, value, cluster in zip(epochs, snapshots, file_ids, values, indexes)]
+        all_metrics = []
+        for n_cluster in range(n_clusters-1):
+            metrics = {value:idx for idx, (value, cluster) in enumerate(zip(values, indexes)) if n_cluster == cluster}
+            out_freq = 1
+            cluster_metrics = list(metrics.keys())
+            max_idx = metrics[np.min(cluster_metrics)]
+            max_traj = paths[max_idx]
+            max_snapshot = snapshots[max_idx]
+            output_traj = files_out[max_idx]
+            input_traj = file_ids[max_idx]
+            if not self.topology:
+                bs.extract_snapshot_from_pdb(max_traj, input_traj, output, self.topology, max_snapshot, out_freq, output_traj)
+            else:
+                bs.extract_snapshot_from_xtc(max_traj, input_traj, output, self.topology, max_snapshot, out_freq, output_traj)
+            all_metrics.append(cluster_metrics)
+        fig, ax = plt.subplots()
+        ax.boxplot(all_metrics)
+        ax.set_xlabel(metric)
+        plt.savefig(os.path.join(output, "clusters_{}_boxplot.png".format(metric)))
+
+
+
+
     def _get_column_name(self, df, column_digit):
         return list(df)[int(column_digit)-1]
 
 
 def analyse_simulation(report_name, traj_name, simulation_path):
-    analysis = PostProcessor(report_name, traj_name, simulation_path)
+    analysis = PostProcessor(report_name, traj_name, simulation_path, 15, residue="BEN")
+
     metrics = len(list(analysis.data)) - 1 #Discard epoch as metric
     sasa = 6
     be = 5
     total_energy = 4
     current_metric = sasa
+
+    # Plot metrics
     while current_metric <= metrics-1:
         try:
-            analysis.plot_two_metrics(be, total_energy, current_metric)
-            analysis.plot_two_metrics(be, current_metric)
+            analysis.plot_two_metrics(be, total_energy, current_metric, output_folder="Plots")
+            analysis.plot_two_metrics(be, current_metric, output_folder="Plots")
         except ValueError:
             break
         current_metric += 1
 
-    analysis.top_poses(be, 100)
+    #Retrieve 100 best structures
+    analysis.top_poses(be, 100, "BestStructs")
+
+    #Clustering of best 2000 best structures
+    analysis.cluster_poses(2000, be, "clusters")
+
+
 
 
 if __name__ == "__main__":
-    analyse_simulation("*report*", "*trajectory*", "/work/NBD_Utilities/PELE/PELE_Softwares/pele_platform/tests/STR_Pele/output/")
+    analyse_simulation("report_", "trajectory_", "/scratch/jobs/dsoler/water/trypsin/BEN_Pele_36/output/")
 
 
