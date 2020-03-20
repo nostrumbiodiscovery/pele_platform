771a9f95758f3ce7409eb10901b5bf68191ae12a
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2020-03-20 01:12:21 +0100

    Refactor frag module

--- pele_platform/Frag/fragments.py
+++ pele_platform/Frag/fragments.py
@@ -6,32 +6,30 @@
 class Fragment():
 
 
-    def __init__(self, file, atom_fragment_idx, hydrogen_added_idx, atom_core_name, hydrogen_core_name, no_hydrogen):
+    def __init__(self, file, atom_fragment, hydrogen_fragment, atom_core, hydrogen_core, no_hydrogen):
         from rdkit import Chem
         import rdkit.Chem.rdchem as rc
         self.file = file
-        self.atom_fragment_idx = atom_fragment_idx
-        self.hydrogen_added_idx = hydrogen_added_idx
-        self.atom_core_name = atom_core_name.strip()
-        self.hydrogen_core_name = hydrogen_core_name.strip()
+        self.atom_fragment = atom_fragment
+        self.hydrogen_fragment = hydrogen_fragment
+        self.atom_core = atom_core
+        self.hydrogen_core = hydrogen_core
         self.mol = Chem.MolFromPDBFile(self.file, removeHs=False)
-        self.atom_fragment_name = self.mol.GetAtomWithIdx(self.atom_fragment_idx).GetMonomerInfo().GetName().strip()
-        self.atom_hydrogen_name = self.mol.GetAtomWithIdx(self.hydrogen_added_idx).GetMonomerInfo().GetName().strip()
         self.no_hydrogen = no_hydrogen
 
 
     def get_inputfile_line(self):
         if self.no_hydrogen:
             self.line = "{} {} {}".format(self.file, 
-                self.atom_core_name,
-                self.atom_fragment_name)
+                self.atom_core.get_name(),
+                self.atom_fragment.get_name())
         else:
             self.line = "{} {}-{} {}-{}".format(self.file, 
-                self.atom_core_name, self.hydrogen_core_name,
-                self.atom_fragment_name, self.atom_hydrogen_name)
+                self.atom_core.get_name(), self.hydrogen_core.get_name(),
+                self.atom_fragment.get_name(), self.hydrogen_fragment.get_name())
         return self.line
 
-    def santize_file(self):
+    def sanitize_file(self):
         with open(self.file, "r") as fin:
             lines = fin.readlines()
         with open(self.file, "w") as fout:
