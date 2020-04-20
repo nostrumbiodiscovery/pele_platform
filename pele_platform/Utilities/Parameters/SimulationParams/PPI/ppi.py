185337661ea0f211ecb8b665b9851dc9ad4d79b7
Anna Gruszka <frumpowy@gmail.com>
2020-04-20 18:25:24 +0200

    PPI simulation parameters

---
+++ pele_platform/Utilities/Parameters/SimulationParams/PPI/ppi.py
@@ -0,0 +1,8 @@
+class PPIParams(object):
+
+
+    def __init__(self, args):
+        self.ppi = args.ppi
+        self.center_of_interface = args.center_of_interface
+        self.n_components = args.n_components if args.n_components else self.simulation_params.get("n_components", 10)
+
