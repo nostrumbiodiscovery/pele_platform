cbfc8d6d92eded1b3cf938f850b2d0d91e8a6a8b
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2020-05-02 17:41:26 +0200

    Detach yaml parser from main and break down frag parameters in sub classes

--- pele_platform/Frag/simulation.py
+++ pele_platform/Frag/simulation.py
@@ -1,45 +1,20 @@
 import os
 import tempfile
 import shutil
-import pele_platform.constants.constants as cs
-import pele_platform.Utilities.Parameters.pele_env as pele
 import pele_platform.Utilities.Helpers.simulation as ad
-import pele_platform.Utilities.Helpers.constraints as cst
 import pele_platform.Frag.helpers as hp
 import pele_platform.Frag.fragments as fr
 import pele_platform.Frag.checker as ch
 import pele_platform.Frag.generative_model as gm
+import pele_platform.Frag.parameters.main as mn
 import pele_platform.Errors.custom_errors as ce
-import PPP.main as ppp
+import frag_pele.main as frag
 
-class FragRunner(pele.EnviroBuilder):
+class FragRunner(mn.FragParameters):
 
     #def __init__(self, core, input, gr_steps, steps, eq_steps):
     def __init__(self, args):
-        self.software = "Frag"
-        self.build_frag_variables(args)
-        self.core = args.frag_core
-        #Prepare complex
-        self.core_process = os.path.basename(ppp.main(self.core, ".", output_pdb=["" , ],
-            charge_terminals=args.charge_ter, no_gaps_ter=args.gaps_ter)[0]) 
-        self.ligands = args.frag_ligands
-        self.ai = args.frag_ai
-        self.ai_iterations = args.frag_ai_iterations
-        self.core_format = args.frag_core.rsplit(".")[-1]
-        self.input = args.frag_input
-        self.gr_steps = args.growing_steps if args.growing_steps else self.simulation_params.get("growing_steps", 6)
-        self.frag_steps = args.frag_steps if args.frag_steps else self.simulation_params.get("steps_in_gs", 3)
-        self.frag_eq_steps = args.frag_eq_steps if args.frag_eq_steps else self.simulation_params.get("sampling_steps", 20)
-        self.control_file = os.path.join(cs.DIR, "Templates/pele_template.conf")
-        self.protocol = args.protocol if args.protocol else self.simulation_params.get("protocol", "")
-        self.topology = None if self.pdb else os.path.join("output_pele", "topology.pdb")
-        self.constraints = cst.retrieve_constraints(self.core, {}, {}, self.ca_constr)
-        self.chain_core = args.chain_core if args.chain_core else self.simulation_params.get("chain_core", "L")
-        self.box = cs.BOX.format(self.box_radius, self.box_center) if  self.box_radius else ""
-        self.gridres = args.gridres
-        self.frag_restart = "-rst" if args.frag_restart else ""
-        self.args = args
-        
+        mn.FragParameters.__init__(self, args)
 
     def prepare_control_file(self):
         # Create tmp folder with frag control_file 
@@ -54,25 +29,13 @@ class FragRunner(pele.EnviroBuilder):
         return self.control_file
 
     def run(self):
-        # If protocol let frag handle all flags
-        if self.protocol:
-            command = "python -m frag_pele.main -cp {} -sef {} --sch_python {} --contrl {} -nc -d {} -dat {} -doc {} --license {} --cpus {} -{} --c_chain {} --rotamers {} --seed {} {}".format(
-                self.core_process, self.input, self.spython, self.control_file,
-                self.pele_exec, self.pele_data, self.pele_documents, self.license,
-                self.cpus, self.protocol, self.chain_core, self.gridres, self.seed,
-                self.frag_restart)
-        else:
-            # Pass all possible flags
-            command = "python -m frag_pele.main -cp {} -sef {} --sch_python {} --contrl {} -nc -d {} -dat {} -doc {} --license {} --cpus {} --growing_steps {} --steps {} --pele_eq_steps {} --temperature  {} --rotamers {} --c_chain {} --seed {} {}".format(
-                self.core_process, self.input, self.spython, self.control_file,
-                self.pele_exec, self.pele_data, self.pele_documents, self.license,
-                self.cpus, self.gr_steps, self.frag_steps, self.frag_eq_steps, self.temperature,
-                self.gridres, self.chain_core, self.seed, self.frag_restart)
-        print(command)
-        if not self.debug:
-            os.system(command)
-        else:
-            os.system(command + " --debug ")
+            frag.main(self.core_process, self.gr_steps, self.criteria, self.plop_path, self.spython, self.pele_exec, self.control_file, self.license, self.output_folder,
+         self.report_name, self.traj_name, self.cluster_folder, self.cpus, self.distcont, self.threshold, self.epsilon, self.condition, self.metricweights,
+         self.nclusters, self.frag_eq_steps, self.frag_restart, self.min_overlap, self.max_overlap, self.input,
+         self.chain_core, self.frag_chain, self.frag_steps, self.temperature, self.seed, self.gridres, self.banned, self.limit, self.mae,
+         self.rename, self.threshold_clash, self.steering, self.translation_high, self.rotation_high,
+         self.translation_low, self.rotation_low, self.explorative, self.frag_radius, self.sampling_control, self.pele_data, self.pele_documents,
+         self.only_prepare, self.only_grow, self.no_check, self.debug)
 
     def prepare_input_file(self):
         from rdkit import Chem
@@ -136,22 +99,6 @@ class FragRunner(pele.EnviroBuilder):
         fragment.sanitize_file()
         return line, fragment
         
-
-    def grow_ai(self):
-        with open(self.core, "r") as fin:
-            ligand_lines = [line for line in fin if line[17:20] == self.residue or line.startswith("CONECT")]
-            ligand_lines.append("END")
-        self.ai = "core.pdb"
-        with open(self.ai, "w") as fout:
-            fout.write("".join(ligand_lines))
-            
-           
-        generative_model_rnn = gm.GenerativeModel(self.ai, self.residue, self.ai_iterations)
-        ligands = generative_model_rnn.run()
-        for ligand in ligands:
-            self.ligands = ligand
-            self.prepare_input_file()
-            self.run()
 
     def set_test_variables(self):
         self.gr_steps = 1
