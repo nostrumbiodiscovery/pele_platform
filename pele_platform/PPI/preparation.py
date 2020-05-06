6fafc43c651a08b18debd5ce61f9e3fe8ae568bf
frumpowy <frumpowy@gmail.com>
2020-05-06 10:27:58 +0200

    Separated water function from preparation

--- pele_platform/PPI/preparation.py
+++ pele_platform/PPI/preparation.py
@@ -1,48 +1,23 @@
-from Bio.PDB import PDBParser, PDBIO, Selection, NeighborSearch, rotmat, Vector
-import numpy as np
+from Bio.PDB import PDBParser, PDBIO, Selection, NeighborSearch
 import os
 
 
 def prepare_structure(protein_file, ligand_pdb, chain):
     
     to_remove = []
-    resnums = []
-    atomnums = []
 
-    water1_O = "HETATM {}  O   HOH {} {}     -25.445  -9.362   2.160  1.00  0.00           O\n"
-    water1_H1 = "HETATM {}  H1  HOH {} {}     -24.805  -8.727   2.791  1.00  0.00           H\n"
-    water1_H2 = "HETATM {}  H2  HOH {} {}     -26.081  -9.993   2.799  1.00  0.00           H\n"
-    water2_O = "HETATM {}  O   HOH {} {}     -41.331 -14.611   0.035  1.00  0.00           O\n"
-    water2_H1 = "HETATM {}  H1  HOH {} {}     -40.692 -13.976   0.666  1.00  0.00           H\n"
-    water2_H2 = "HETATM {}  H2  HOH {} {}     -41.966 -15.243   0.674  1.00  0.00           H\n"
-
-    water = [water1_O, water1_H1, water1_H2, water2_O, water2_H1, water2_H2]
-
-    # remove additional protein chains and adjust water
+    # remove additional protein chains and all water molecules
     with open(protein_file, "r") as file:
         lines = file.readlines()
+
         for line in lines:
-            if ((line.startswith("ATOM") or line.startswith("HETATM")) and line[21:22].strip() not in chain) or line.startswith("END") or line.startswith("TER") or line.startswith("CONECT"):
+            if ((line.startswith("ATOM") or line.startswith("HETATM")) and line[21:22].strip() not in chain) or \
+                    line.startswith("END") or line.startswith("TER") or line.startswith("CONECT"):
                 to_remove.append(line)
             if (line.startswith("ATOM") or line.startswith("HETATM")) and line[17:20].strip() == "HOH":
-                    to_remove.append(line)
+                to_remove.append(line)
+
         protein = [line for line in lines if line not in to_remove]
-
-    for line in protein:
-        if line.startswith("ATOM") or line.startswith("HETATM"):
-            resnums.append(line[23:27].strip())
-            atomnums.append(line[7:11].strip())
-
-    # adding water
-    water_chain = chain 
-    resnums = [int(num) for num in resnums]
-    water_resnums = [(max(resnums)+1)]*3+[(max(resnums)+2)]*3
-    atomnum = max([int(num) for num in atomnums])
-    water_atomnums = range(atomnum+1, atomnum+7)
-
-    water_output = []
-    for atom, num, resnum in zip(water, water_atomnums, water_resnums):
-        water_output.append(atom.format(num, water_chain, resnum))
     
     # read in ligand file
     ligand = []
@@ -55,46 +30,98 @@ def prepare_structure(protein_file, ligand_pdb, chain):
     new_protein_file = os.path.basename(protein_file).replace(".pdb", "_prep.pdb")
     new_protein_file = os.path.abspath(new_protein_file)
     
-    # join protein and ligand pdbs into new file
+    # join protein and ligand PDBs into new file
     with open(new_protein_file, "w+") as file:
         for line in protein:
             file.write(line)
         file.write("\n")
-        for line in water_output:
-            file.write(line)
-        file.write("\n")
         for line in ligand:
             file.write(line)
-
-    # load again with biopython
-    parser = PDBParser()
-    structure = parser.get_structure("complex", new_protein_file)
-    water_list = []
-    protein_list = Selection.unfold_entities(structure, "A")
-
-    for res in structure.get_residues():
-        if res.resname == 'HOH':
-            water_list = water_list + Selection.unfold_entities(res, "A")
-
-    # check for water contacts
-    contacts5 = []
-    for w in water_list:
-        contacts5 = contacts5 + NeighborSearch(protein_list).search(w.coord, 5.0, "A")
-    contacts5 = [c for c in contacts5 if c not in water_list]
-    
-    # translate water, if needed
-    while contacts5:
-        contacts5 = []
-        for w in water_list:
-            x, y, z = w.coord
-            w.set_coord([x-5, y, z])
-            contacts5 = contacts5 + NeighborSearch(protein_list).search(w.coord, 5.0, "A") 
-            contacts5 = [c for c in contacts5 if c not in water_list] 
-    
-    # save final output
-    io = PDBIO()
-    io.set_structure(structure)
-    io.save(new_protein_file)
  
     return new_protein_file
 
+
+def add_water(refinement_input, chain):
+
+    output = []
+
+    # hard-coded coordinates of water molecules
+    water1_O = "HETATM {}  O   HOH {} {}     -25.445  -9.362   2.160  1.00  0.00           O\n"
+    water1_H1 = "HETATM {}  H1  HOH {} {}     -24.805  -8.727   2.791  1.00  0.00           H\n"
+    water1_H2 = "HETATM {}  H2  HOH {} {}     -26.081  -9.993   2.799  1.00  0.00           H\n"
+    water2_O = "HETATM {}  O   HOH {} {}     -41.331 -14.611   0.035  1.00  0.00           O\n"
+    water2_H1 = "HETATM {}  H1  HOH {} {}     -40.692 -13.976   0.666  1.00  0.00           H\n"
+    water2_H2 = "HETATM {}  H2  HOH {} {}     -41.966 -15.243   0.674  1.00  0.00           H\n"
+
+    water = [water1_O, water1_H1, water1_H2, water2_O, water2_H1, water2_H2]
+
+    resnums = []
+    atomnums = []
+
+    # get maximum residue and atom numbers
+    with open(refinement_input[0], "r") as file:
+        protein = file.readlines()
+
+        for line in protein:
+            if line.startswith("ATOM") or line.startswith("HETATM"):
+                resnums.append(line[23:27].strip())
+                atomnums.append(line[7:11].strip())
+
+    water_chain = chain  # from function args
+    resnums = [int(num) for num in resnums]
+    water_resnums = [(max(resnums)+1)]*3+[(max(resnums)+2)]*3
+    atomnum = max([int(num) for num in atomnums])
+    water_atomnums = range(atomnum+1, atomnum+7)
+
+    # PDB lines - water
+    water_output = []
+    for atom, num, resnum in zip(water, water_atomnums, water_resnums):
+        water_output.append(atom.format(num, water_chain, resnum))
+
+    # loop over minimisation inputs
+    for inp in refinement_input:
+
+        new_protein_file = os.path.basename(inp).replace(".pdb", "_water.pdb")
+        new_protein_file = os.path.abspath(new_protein_file)
+
+        # add water to PDB
+        with open(new_protein_file, "w+") as file:
+            for line in protein:
+                file.write(line)
+            file.write("\n")
+            for line in water_output:
+                file.write(line)
+            file.write("\n")
+
+        # load again with Biopython
+        parser = PDBParser()
+        structure = parser.get_structure("complex", new_protein_file)
+        water_list = []
+        protein_list = Selection.unfold_entities(structure, "A")
+
+        for res in structure.get_residues():
+            if res.resname == 'HOH':
+                water_list = water_list + Selection.unfold_entities(res, "A")
+
+        # check for water contacts
+        contacts5 = []
+        for w in water_list:
+            contacts5 = contacts5 + NeighborSearch(protein_list).search(w.coord, 5.0, "A")
+        contacts5 = [c for c in contacts5 if c not in water_list]  # exclude "self" contacts
+
+        # translate water, if needed
+        while contacts5:
+            contacts5 = []
+            for w in water_list:
+                x, y, z = w.coord
+                w.set_coord([x - 5, y, z])
+                contacts5 = contacts5 + NeighborSearch(protein_list).search(w.coord, 5.0, "A")
+                contacts5 = [c for c in contacts5 if c not in water_list]
+
+        # save final output
+        io = PDBIO()
+        io.set_structure(structure)
+        io.save(new_protein_file)
+        output.append(new_protein_file)
+
+    return output
