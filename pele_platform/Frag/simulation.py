1b4f206e68c4beeadf6419d04eac5038719f56ac
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2020-05-07 05:55:51 +0200

    Fix bug on serie_file positional parameter wrongly placed

--- pele_platform/Frag/simulation.py
+++ pele_platform/Frag/simulation.py
@@ -30,9 +30,9 @@ class FragRunner(mn.FragParameters):
 
     def run(self):
          if self.frag_run:
-             frag.main(self.core_process, self.gr_steps, self.criteria, self.plop_path, self.spython, self.pele_exec, self.control_file, self.license, self.output_folder,
+             frag.main(self.core_process, self.input, self.gr_steps, self.criteria, self.plop_path, self.spython, self.pele_exec, self.control_file, self.license, self.output_folder,
              self.report_name, self.traj_name, self.cluster_folder, self.cpus, self.distcont, self.threshold, self.epsilon, self.condition, self.metricweights,
-             self.nclusters, self.frag_eq_steps, self.frag_restart, self.min_overlap, self.max_overlap, self.input,
+             self.nclusters, self.frag_eq_steps, self.frag_restart, self.min_overlap, self.max_overlap,
              self.chain_core, self.frag_chain, self.frag_steps, self.temperature, self.seed, self.gridres, self.banned, self.limit, self.mae,
              self.rename, self.threshold_clash, self.steering, self.translation_high, self.rotation_high,
              self.translation_low, self.rotation_low, self.explorative, self.frag_radius, self.sampling_control, self.pele_data, self.pele_documents,
