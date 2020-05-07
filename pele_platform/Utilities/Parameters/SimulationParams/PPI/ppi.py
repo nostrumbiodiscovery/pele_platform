1c4f9a7ae12aa574b3b130de7747ce1b28f3cfdc
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2020-05-07 06:03:46 +0200

    Chang default clusters of PPi from 10 to 25

--- pele_platform/Utilities/Parameters/SimulationParams/PPI/ppi.py
+++ pele_platform/Utilities/Parameters/SimulationParams/PPI/ppi.py
@@ -4,5 +4,5 @@ class PPIParams(object):
     def __init__(self, args):
         self.ppi = args.ppi
         self.center_of_interface = args.center_of_interface
-        self.n_components = args.n_components if args.n_components else self.simulation_params.get("n_components", 10)
+        self.n_components = args.n_components if args.n_components else self.simulation_params.get("n_components", 25)
 
