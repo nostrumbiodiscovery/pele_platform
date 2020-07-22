972fd82c0e83bed858e87728333b69aa32195312
danielSoler93 <daniel.soler@e-campus.uab.cat>
2020-07-22 19:43:04 +0200

    Avoid repited key cmdclass

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from distutils.extension import Extension
 
 here = path.abspath(path.dirname(__file__))
 ext_modules = []
-cmdclass = {}
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
      long_description = f.read()
@@ -26,6 +25,5 @@ setup(
     include_package_data=True,
     install_requires=["AdaptivePELE", "frag_pele>=2.1.1", "lib_prep", "PPPele==1.0.4", "PlopRotTemp==1.0.1", "PyYAML", "fpdf", "scikit-learn>0.2", "pillow", "scipy", "matplotlib", 
        "biopython", "pandas", "pytest", "cython", "numpy"],    
-    cmdclass=cmdclass,
     ext_modules=ext_modules  # accepts a glob pattern
 )
