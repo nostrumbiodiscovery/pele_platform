29c35e9c31534b3db3a6dcaffcb5d2e38b16b5de
danielSoler93 <daniel.soler@e-campus.uab.cat>
2020-09-22 19:53:37 +0200

    Update docs to 1.5.1 and change singularity typo

--- docs/build_docs/source/conf.py
+++ docs/build_docs/source/conf.py
@@ -55,7 +55,7 @@ author = 'Daniel Soler Viladrich'
 # built documents.
 #
 # The short X.Y version.
-version = '1.5'
+version = '1.5.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.5.1'
 
