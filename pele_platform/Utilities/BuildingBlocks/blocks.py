043a944ccab920192bcdc93a71b84bbda14e6521
frumpowy <frumpowy@gmail.com>
2020-11-25 14:34:17 +0100

    PELE-274 More work done on BB, moved Pipeline to blocks, created class for input generation

--- pele_platform/Utilities/BuildingBlocks/blocks.py
+++ pele_platform/Utilities/BuildingBlocks/blocks.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-import shutil
 import numpy as np
 import os
 import pandas as pd
@@ -11,44 +10,25 @@ from pele_platform.Utilities.Helpers.helpers import is_repeated, is_last, cd, pa
 import pele_platform.Utilities.Parameters.pele_env as pv
 
 
-"""COMMUN CLASSS"""
-class Linker():
-
-    def finish_state(value):
-        self.env.next_step = value
-
-    def set_params():
-
-
 @dataclass
-class GlobalExploration(Linker):
+class Simulation:
+    """
+    Base Simulation class to set parameters, define input for the next BuildingBlock, etc.
+    Both input and output should always be EnviroBuilder type.
 
-
+    One class to rule them all, one class to find them, one class to bring them all and in PELE bind them.
+    """
     env: pv.EnviroBuilder
 
-    def run(self):
-        """
-        Launch global exploration.
-        """
-        self.set_params()
-        self.set_working_folder()
-        simulation_params = si.run_adaptive(self.env)
-        #self.finish_state()
-        return simulation_params
-
-
-    #####INherit from parent class so you don't have to repeat########
-    def set_params(self, package="global"):
-        """
-        Set parameters for global exploration.
-        """
-        ###convert to one that control them all!!!
-        self.env.full = package
-        self.env.type_simulation = package
-        ###create_files_and_folders
+    def set_params(self, simulation_type):
+        self.env.type_simulation = simulation_type
         self.env.create_files_and_folders()
+        self.set_working_folder(folder_name=None)
 
-    def set_working_folder(self):
+    def finish_state(self, value):
+        self.env.next_step = value
+
+    def set_working_folder(self, folder_name):
         """
         Set working folder. Users can pick their own.
         """
@@ -59,125 +39,140 @@ class GlobalExploration(Linker):
                 working_folder)
         else:
             self.env.working_folder = os.path.abspath(self.env.folder)
-        self.env.folder = os.path.join(self.env.working_folder, "1_global_exploration")
+        self.env.folder = os.path.join(self.env.working_folder, folder_name)
 
 
 @dataclass
-class InducedFitFast:
+class GlobalExploration(Simulation):
+    env: pv.EnviroBuilder
+    folder_name: str
 
-    args: pv.EnviroBuilder
+    def run(self):
+        """
+        Launch global exploration.
+        """
+        self.set_params(simulation_type="full")
+        self.set_working_folder(self.folder_name)
+        simulation_params = si.run_adaptive(self.env)
+        self.finish_state(simulation_params.output)
+        return simulation_params
+
+
+@dataclass
+class InducedFitFast(Simulation):
+    env: pv.EnviroBuilder
+    folder_name: str
 
     def run(self):
         """
         Launch induced fit fast.
         """
-        self.set_params()
-        choose_refinement_input(self.args)
-        simulation_params = si.run_adaptive(self.args)
+        self.set_params(simulation_type="induced_fit_fast")
+        self.set_working_folder(self.folder_name)
+        simulation_params = si.run_adaptive(self.env)
+        self.finish_state(simulation_params.output)
         return simulation_params
-
-    def set_params(self):
-        """
-        Set induced fit params making sure there are no arguments from the previous simulation (building block).
-        """
-        self.args.full = False
-        self.args.induced_fit_fast = True
 
 
 @dataclass
-class InducedFitExhaustive:
+class InducedFitExhaustive(Simulation):
     """
     Launch induced fit exhaustive.
     """
-    args: pv.EnviroBuilder
+    env: pv.EnviroBuilder
+    folder_name: str
 
     def run(self):
-        self.set_working_folder()
-        self.set_params()
-        choose_refinement_input(self.args)
-        simulation_params = si.run_adaptive(self.args)
+        self.set_params(simulation_type="induced_fit_exhaustive")
+        self.set_working_folder(self.folder_name)
+        simulation_params = si.run_adaptive(self.env)
+        self.finish_state(simulation_params.output)
         return simulation_params
-
-    def set_params(self):
-        self.args.full = None  # in case it was passed from previous building block
-        self.args.poses = None
-        self.args.induced_fit_exhaustive = True
-        self.args.system = os.path.join(self.args.working_folder, "induced_fit_input/*.pdb")
-
-        if not self.args.test:
-            self.args.iterations = 20
-
-    def set_working_folder(self):
-        self.args.working_folder = os.path.dirname(self.args.folder)
-        self.args.folder = os.path.join(self.args.working_folder, "2_induced_fit_exhaustive")
 
 
 @dataclass
-class Rescoring:
-
-    args: pv.EnviroBuilder
+class Rescoring(Simulation):
+    """
+    Launch rescoring simulation.
+    """
+    env: pv.EnviroBuilder
+    folder_name: str
 
     def run(self):
-        self.set_params()
-        choose_refinement_input(self.args)
-        simulation_params = si.run_adaptive(self.args)
+        self.set_params(simulation_type="rescoring")
+        self.set_working_folder(self.folder_name)
+        simulation_params = si.run_adaptive(self.env)
+        self.finish_state(simulation_params.output)
         return simulation_params
 
-    def set_params(self):
-        pass
 
+@dataclass
+class Selection:
+    simulation_params: pv.EnviroBuilder
 
-def choose_refinement_input(simulation_params):
-    """
-    Choose input for refinement simulation.
-    Scan top 75% binding energies, pick n best ones as long as ligand COMs are >= box radius away from each other.
-    """
-    simulation_path = os.path.join(simulation_params.pele_dir, simulation_params.output)
-    n_best_poses = int(simulation_params.iterations * simulation_params.pele_steps * (
-            simulation_params.cpus - 1) * 0.75)
+    def choose_refinement_input(self):
+        """
+        Choose input for refinement simulation.
+        Scan top 75% binding energies, pick n best ones as long as ligand COMs are >= box radius away from each other.
+        """
+        simulation_path = os.path.join(self.simulation_params.pele_dir, self.simulation_params.output)
+        n_best_poses = int(self.simulation_params.iterations * self.simulation_params.pele_steps * (
+                self.simulation_params.cpus - 1) * 0.75)
 
-    with cd(simulation_path):
-        files_out, _, _, _, output_energy = bs.main(str(simulation_params.be_column), n_structs=n_best_poses, path=".",
-                                                    topology=simulation_params.topology,
-                                                    logger=simulation_params.logger)
+        with cd(simulation_path):
+            files_out, _, _, _, output_energy = bs.main(str(self.simulation_params.be_column), n_structs=n_best_poses,
+                                                        path=".",
+                                                        topology=self.simulation_params.topology,
+                                                        logger=self.simulation_params.logger)
 
-        snapshot = 0
-        files_out = [os.path.join(simulation_params.pele_dir, "results", f) for f in files_out]
-        input_pool = [[f, snapshot, simulation_params.residue, simulation_params.topology] for f in
-                      files_out]
-        all_coords = parallelize(_extract_coords, input_pool, simulation_params.cpus)
-        coords = [list(c[0:3]) for c in all_coords]
-        dataframe = pd.DataFrame(list(zip(files_out, output_energy, coords)),
-                                 columns=["File", "Binding energy", "1st atom coordinates"])
-        dataframe = dataframe.sort_values(["Binding energy"], ascending=True)
+            snapshot = 0
+            files_out = [os.path.join(self.simulation_params.pele_dir, "results", f) for f in files_out]
+            input_pool = [[f, snapshot, self.simulation_params.residue, self.simulation_params.topology] for f in
+                          files_out]
+            all_coords = parallelize(_extract_coords, input_pool, self.simulation_params.cpus)
+            coords = [list(c[0:3]) for c in all_coords]
+            dataframe = pd.DataFrame(list(zip(files_out, output_energy, coords)),
+                                     columns=["File", "Binding energy", "1st atom coordinates"])
+            dataframe = dataframe.sort_values(["Binding energy"], ascending=True)
 
-        inputs = _check_ligand_distances(simulation_params, dataframe)
-        directory = os.path.join(simulation_params.working_folder, "refinement_input")
+            inputs = self._check_ligand_distances(self.simulation_params, dataframe)
+            directory = os.path.join(self.simulation_params.folder, "refinement_input")
 
-        if not os.path.isdir(directory):
-            os.makedirs(directory, exist_ok=True)
-        for i in inputs:
-            os.system("cp {} {}/.".format(i, directory))
+            if not os.path.isdir(directory):
+                os.makedirs(directory, exist_ok=True)
+            for i in inputs:
+                os.system("cp {} {}/.".format(i, directory))
 
+    def _check_ligand_distances(self, dataframe):
 
-def _check_ligand_distances(simulation_params, dataframe):
+        inputs = []
+        input_coords = []
+        distances = []
+        n_inputs = self.simulation_params.cpus - 1
 
-    inputs = []
-    input_coords = []
-    distances = []
-    n_inputs = simulation_params.cpus - 1
-
-    while len(inputs) < n_inputs:
-        for f, c in zip(dataframe['File'], dataframe['1st atom coordinates']):
-            if not input_coords:
-                inputs.append(f)
-                input_coords.append(c)
-            else:
-                for ic in input_coords:
-                    distances.append(abs(np.linalg.norm(np.array(c) - np.array(ic))))
-                distances_bool = [d > simulation_params.box_radius for d in distances]
-                if all(distances_bool):
+        while len(inputs) < n_inputs:
+            for f, c in zip(dataframe['File'], dataframe['1st atom coordinates']):
+                if not input_coords:
                     inputs.append(f)
                     input_coords.append(c)
-        break  # make sure it stops after running out of files to check
-    return inputs
+                else:
+                    for ic in input_coords:
+                        distances.append(abs(np.linalg.norm(np.array(c) - np.array(ic))))
+                    distances_bool = [d > self.simulation_params.box_radius for d in distances]
+                    if all(distances_bool):
+                        inputs.append(f)
+                        input_coords.append(c)
+            break  # make sure it stops after running out of files to check
+        return inputs
+
+
+@dataclass
+class Pipeline:
+
+    def __init__(self, iterable, env):
+        self.iterable = iterable
+        self.env = env
+
+        for simulation in self.iterable:
+            folder = "Simulation_step_{}".format(self.iterable.index(simulation) + 1)
+            self.env = simulation(self.env, folder).run()
