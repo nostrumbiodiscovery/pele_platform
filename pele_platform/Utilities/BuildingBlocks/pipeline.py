1667f1cf4d6a9e04a21fe16c19e05b0144e09f5e
frumpowy <frumpowy@gmail.com>
2020-12-10 15:48:46 +0100

    PELE-274 Added debug flag support to Pipeline

--- pele_platform/Utilities/BuildingBlocks/pipeline.py
+++ pele_platform/Utilities/BuildingBlocks/pipeline.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 from dataclasses import dataclass
-
 from pele_platform.Utilities.Parameters import pele_env as pv
 
 
@@ -10,10 +9,19 @@ class Pipeline:
     env: pv.EnviroBuilder
 
     def run(self):
+        self.check_debug()
         output = []
         for simulation in self.iterable:
             folder = "{}_{}".format(self.iterable.index(simulation) + 1, simulation.__name__)
             self.env = simulation(self.env, folder).run()
             output.append(deepcopy(self.env))
-
         return output
+
+    def check_debug(self):
+        """
+        If input.yaml contains debug: true flag, the execution of the Pipeline should stop after setting up
+        parameters and folders for the first simulation.
+        """
+        if self.env.initial_args.debug:
+            self.env.debug = self.env.initial_args.debug
+            self.iterable = [self.iterable[0]]  # 1st simualtion in the Pipeline only
