5f3d9d100e1cd86601a4fd28e85723a82bcff27f
frumpowy <frumpowy@gmail.com>
2020-12-14 16:57:00 +0100

    PELE-274 Moved protein preparation for PPI

--- pele_platform/PPI/main.py
+++ pele_platform/PPI/main.py
@@ -1,6 +1,5 @@
 from pele_platform.Utilities.BuildingBlocks.pipeline import Pipeline
 from pele_platform.Utilities.BuildingBlocks.selection import Scatter6
-from pele_platform.Utilities.BuildingBlocks.preparation import Preparation
 from pele_platform.Utilities.BuildingBlocks.blocks import Rescoring, InducedFitExhaustive
 
 
@@ -13,7 +12,7 @@ class PPILauncher:
         self.env.package = "ppi"
 
         if not self.env.initial_args.skip_refinement:
-            result = Pipeline([Preparation, InducedFitExhaustive, Scatter6, Rescoring], self.env).run()
+            result = Pipeline([InducedFitExhaustive, Scatter6, Rescoring], self.env).run()
         else:
-            result = Pipeline([Preparation, InducedFitExhaustive], self.env).run()
+            result = Pipeline([InducedFitExhaustive], self.env).run()
         return result
