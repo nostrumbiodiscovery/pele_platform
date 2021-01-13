de4346f29c96d22a380d87a82aa9e7c9bbd1d5a9
frumpowy <frumpowy@gmail.com>
2021-01-13 23:18:52 +0100

    PELE-274 Support for optional params (nested yaml) - tests pass

--- pele_platform/out_in/main.py
+++ pele_platform/out_in/main.py
@@ -4,4 +4,4 @@ from pele_platform.Utilities.Helpers.launcher_base import LauncherBase
 class OutInLauncher(LauncherBase):
     steps = [{'type': 'OutIn'},
              {'type': 'ScatterN', 'options': {'distance': 6.0}},
-             {{'type': 'Rescoring'}}]
+             {'type': 'Rescoring'}]
