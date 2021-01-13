c9828688f266fa8828ef7e25c6e9951c68f68338
frumpowy <frumpowy@gmail.com>
2021-01-13 19:31:15 +0100

    PELE-274 Added optional params to fixtures

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -58,7 +58,8 @@ Rescoring_lines = [
     '"conditions": [1, 0.6, 0.4, 0.0]'
 ]
 
-Scatter6_inputs = ['/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch0_trajectory_1.1_BindingEnergy-107.584.pdb']
+Scatter6_inputs = [
+    '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch0_trajectory_1.1_BindingEnergy-107.584.pdb']
 
 LowestEnergy5_inputs = [
     '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch0_trajectory_1.1_BindingEnergy-107.584.pdb',
@@ -69,9 +70,7 @@ LowestEnergy5_inputs = [
 
 GMM_inputs = [
     '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch1_trajectory_2.1_BindingEnergy-64.1978.pdb',
- 
-    
-    
+
     '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch0_trajectory_1.1_BindingEnergy-107.584.pdb',
     '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch1_trajectory_1.5_BindingEnergy-62.9806.pdb',
     '/home/agruzka/work_pele_platform/pele_platform/Examples/Blocks/mock_simulation/results/BestStructs/epoch0_trajectory_3.4_BindingEnergy-43.7304.pdb'
@@ -85,7 +84,8 @@ Clusters_inputs = [
 ]
 
 
-@pytest.mark.parametrize("iterable", [([ScatterN, bb.Rescoring]), ([bb.GPCR, bb.Rescoring]), ([])])
+@pytest.mark.parametrize("iterable", [([{'type': 'ScatterN', 'options': {'distance': 6.0}}, {'type': 'Rescoring'}]),
+                                      ([{'type': 'GPCR'}, {'type': 'Rescoring'}]), ([])])
 def test_pipeline_checker(iterable):
     try:
         env = pv.EnviroBuilder()
@@ -100,27 +100,27 @@ def test_pipeline_checker(iterable):
                          [("gpcr/input_defaults.yaml", "gpcr_orth", bb.GPCR, GPCR_lines),
                           ("global/input_defaults.yaml", "global", bb.GlobalExploration, GlobalExploration_lines),
                           ("out_in/input_default.yaml", "out_in", bb.OutIn, OutIn_lines),
-                          ("induced_fit/input_exhaustive_defaults.yaml", "induced_fit_exhaustive", bb.InducedFitExhaustive,
+                          ("induced_fit/input_exhaustive_defaults.yaml", "induced_fit_exhaustive",
+                           bb.InducedFitExhaustive,
                            InducedFitExhaustive_lines),
                           ("induced_fit/input_fast_defaults.yaml", "induced_fit_fast", bb.InducedFitFast,
                            InducedFitFast_lines),
                           ("rescoring/input_defaults.yaml", "rescoring", bb.Rescoring, Rescoring_lines)])
 def test_simulation_blocks(yaml, package, block, expected):
-    
     # get YamlParser ready
     yaml_file = os.path.join(test_path, yaml)
     yaml = yp.YamlParser(yaml_file, vf.VALID_FLAGS_PLATFORM)
     yaml.read()
-    
+
     # create pele environment
     pele_env = pv.EnviroBuilder()
     pele_env.initial_args = yaml
     pele_env.initial_args.package = pele_env.package = package
-    
+
     # run Building Block
-    simulation_block = block(pele_env, "test_folder")
+    simulation_block = block(pele_env, {}, "test_folder")
     params = simulation_block.run()
-    
+
     directory = params.pele_dir
     errors = []
     errors = tk.check_file(directory, "adaptive.conf", expected, errors)
@@ -129,15 +129,15 @@ def test_simulation_blocks(yaml, package, block, expected):
 
 @pytest.fixture
 def mock_simulation_env():
-    env = pv.EnviroBuilder()                                                                                                                                                            
-    env.pele_dir = os.path.join(test_path, "Blocks/mock_simulation")                                                                                                                    
-    env.output = "output"                                                                                                                                                               
-    env.iterations = 2                                                                                                                                                                  
-    env.pele_steps = 12                                                                                                                                                                 
-    env.cpus = 5                                                                                                                                                                        
-    env.be_column = 5                                                                                                                                                                   
-    env.topology = None                                                                                                                                                                 
-    env.logger = None                                                                                                                                                                   
+    env = pv.EnviroBuilder()
+    env.pele_dir = os.path.join(test_path, "Blocks/mock_simulation")
+    env.output = "output"
+    env.iterations = 2
+    env.pele_steps = 12
+    env.cpus = 5
+    env.be_column = 5
+    env.topology = None
+    env.logger = None
     env.residue = "LIG"
 
     selection_path = os.path.join(os.path.dirname(env.pele_dir), "test_Selection")
@@ -146,12 +146,14 @@ def mock_simulation_env():
     return env
 
 
-@pytest.mark.parametrize(("selection_block", "expected"), [(ScatterN, Scatter6_inputs), (LowestEnergy5, LowestEnergy5_inputs), (GMM, GMM_inputs), (Clusters, Clusters_inputs)])
-def test_selection_blocks(mock_simulation_env, selection_block, expected):
-
-    selection = selection_block(mock_simulation_env, "test_folder")
+@pytest.mark.parametrize(("selection_block", "options", "expected"), [(ScatterN, {'distance': 6.0}, Scatter6_inputs),
+                                                                      (LowestEnergy5, None, LowestEnergy5_inputs),
+                                                                      (GMM, None, GMM_inputs),
+                                                                      (Clusters, None, Clusters_inputs)])
+def test_selection_blocks(mock_simulation_env, selection_block, options, expected):
+    selection = selection_block(mock_simulation_env, options, "test_folder")
     selection.run()
-    
+
     assert selection.inputs == expected
 
 
@@ -161,7 +163,7 @@ def test_workflow():
     rescoring_params = output[-1]
     rescoring_output = os.path.join(rescoring_params.pele_dir, rescoring_params.output, "*/trajectory*.pdb")
     output_files = glob.glob(rescoring_output)
-    
+
     assert os.path.exists(rescoring_params.pele_dir)
     assert output_files
 
@@ -170,9 +172,9 @@ def test_workflow_checker():
     yaml = os.path.join(test_path, "Blocks/input_wrong_workflow.yaml")
     try:
         output = main.run_platform(yaml)
-    except ce.PipelineError:                                                                                                                                                            
-        assert True                                                                                                                                                                     
-        return 
+    except ce.PipelineError:
+        assert True
+        return
     assert False
 
 
@@ -183,3 +185,4 @@ def test_optional_params():
     assert induced.box_radius == 10.0
     assert scatter.distance == 3.0
     assert rescoring.box_radius == 5.0
+
