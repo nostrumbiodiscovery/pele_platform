d7a58f03fb101b59a9820927d32ddacd5479c4f2
frumpowy <frumpowy@gmail.com>
2021-01-19 23:37:25 +0100

    PELE-274 Moved all package launchers to DrugDesign

--- pele_platform/GPCR/main.py
+++
@@ -1,9 +0,0 @@
-from pele_platform.Utilities.Helpers.launcher_base import LauncherBase
-
-
-class GPCRLauncher(LauncherBase):
-
-    steps = [
-        {'type': 'GPCR'},
-        {'type': 'ScatterN', 'options': {"distance": 6.0}},
-        {'type': 'Rescoring'}]
--- pele_platform/out_in/main.py
+++
@@ -1,7 +0,0 @@
-from pele_platform.Utilities.Helpers.launcher_base import LauncherBase
-
-
-class OutInLauncher(LauncherBase):
-    steps = [{'type': 'OutIn'},
-             {'type': 'ScatterN', 'options': {'distance': 6.0}},
-             {'type': 'Rescoring'}]
