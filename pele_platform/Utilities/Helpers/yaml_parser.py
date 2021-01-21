106f95356e454d547c003fa4d6fcb813c47b7661
Chari Gago <rosario.gago@nostrumbiodiscovery.com>
2021-01-21 16:06:49 +0100

    PELE-239: Add new flag to YAML - structural_constraints

--- pele_platform/Utilities/Helpers/yaml_parser.py
+++ pele_platform/Utilities/Helpers/yaml_parser.py
@@ -237,6 +237,7 @@ class YamlParser(object):
         self.water_empty_selector = data.get(valid_flags["water_empty_selector"], False)
         self.polarize_metals = data.get(valid_flags["polarize_metals"], False)
         self.polarization_factor = data.get(valid_flags["polarization_factor"], 2)
+        self.structural_constraints = data.get(valid_flags["structural_constraints"], None)
 
         # Metal constraints
         self.permissive_metal_constr = data.get(
