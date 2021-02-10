df7e6ca96c0ab160f1baa759b2b23dcd498ae49c
Anna Gruszka <frumpowy@gmail.com>
2021-02-10 17:13:30 +0100

    PELE-413 Changed global pele_params

--- pele_platform/constants/pele_params.py
+++ pele_platform/constants/pele_params.py
@@ -155,11 +155,15 @@ GLOBAL = '''
                      "otherwise": { "Perturbation::parameters": { "rotationScalingFactor": 0.25 } }
                  },
                  { "ifAnyIsTrue": [ "rand1 >= 0.5" ],
-                     "doThesechanges": { "Perturbation::parameters": { "translationRange": 3.0, "numberOfTrials" : 10 } },
-                     "otherwise": { "Perturbation::parameters": { "translationRange": 1.25, "numberOfTrials" : 10 } }
+                     "doThesechanges": { "Perturbation::parameters": { "translationRange": 2.0, "numberOfTrials" : 10 } },
+                     "otherwise": { "Perturbation::parameters": { "translationRange": 1.0, "numberOfTrials" : 10 } }
+                 },
+                 {  "ifAnyIsTrue": [ "rand2 >= 0.5" ],
+                   "doThesechanges": { "Perturbation::parameters": { "steeringUpdateFrequency": 0 }},
+                   "otherwise": {  "Perturbation::parameters": { "steeringUpdateFrequency": 1  }}
                  },
                  { "ifAnyIsTrue": [ "sasaLig >= 0.85" ],
-                     "doThesechanges": { "Perturbation::parameters": { "translationRange": 5.0, "numberOfTrials" : 20 } },
+                     "doThesechanges": { "Perturbation::parameters": { "translationRange": 3.0, "numberOfTrials" : 20 } },
                      "otherwise": {  }
                  },
                  { "ifAnyIsTrue": [ "sasaLig <= 0.35" ],
