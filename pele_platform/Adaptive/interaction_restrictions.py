e758d43ba0c4aa184a4ae5a41c8c47e42d7f1119
Chari Gago <rosario.gago@nostrumbiodiscovery.com>
2021-02-17 11:20:37 +0100

    PELE-239: Code reformatted with black

--- pele_platform/Adaptive/interaction_restrictions.py
+++ pele_platform/Adaptive/interaction_restrictions.py
@@ -3,23 +3,20 @@ import pele_platform.constants.constants as cs
 
 metrics_config = {
     "distance": {
-            "template": cs.DISTANCE_ATOMS_TAG,
-            "description": "distance",
-            "num_elems": 2
-        },
-    "angle": {
-            "template": cs.ANGLE_ATOMS_TAG,
-            "description": "angle",
-            "num_elems": 3
-    }
+        "template": cs.DISTANCE_ATOMS_TAG,
+        "description": "distance",
+        "num_elems": 2,
+    },
+    "angle": {"template": cs.ANGLE_ATOMS_TAG, "description": "angle", "num_elems": 3},
 }
 
+
 class InteractionRestrictionsBuilder:
-    '''
+    """
     Base class to generate interaction restrictions
     1) Parse interaction restrictions block (metrics and conditions)
     2) Generate json format for pele_template.conf
-    '''
+    """
 
     def __init__(self):
         self.metrics = []
@@ -34,7 +31,6 @@ class InteractionRestrictionsBuilder:
                     self._add_metric(pdb, metrics_config[metric], actual["atoms"], name)
                     self._create_conditions(actual[metric], name)
 
-
     def conditions_to_json(self):
         return cs.INTERACTION_RESTRICTIONS.format('",\n\t"'.join(self.conditions))
 
@@ -45,11 +41,15 @@ class InteractionRestrictionsBuilder:
         num_atoms = config["num_elems"]
         if len(values) != num_atoms:
             raise SyntaxError(
-                "Must specify a list of " + str(num_atoms) + " atoms in " + config["description"] + " restriction."
+                "Must specify a list of "
+                + str(num_atoms)
+                + " atoms in "
+                + config["description"]
+                + " restriction."
             )
         atoms = []
         for i in range(num_atoms):
-            atoms.append(hp.retrieve_atom_info(values[i-1], pdb))
+            atoms.append(hp.retrieve_atom_info(values[i - 1], pdb))
         self.metrics.append(config["template"].format(name, *atoms))
 
     def _create_conditions(self, values, name):
