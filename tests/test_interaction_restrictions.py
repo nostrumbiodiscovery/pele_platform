75e41d2ba8f356d05f87e4c25b5861d3c65162f7
Chari Gago <rosario.gago@nostrumbiodiscovery.com>
2021-03-03 14:32:49 +0100

    PELE-239: More refactor and new test

--- tests/test_interaction_restrictions.py
+++ tests/test_interaction_restrictions.py
@@ -5,11 +5,13 @@ import pele_platform.Adaptive.interaction_restrictions as ir
 import pele_platform.Utilities.Helpers.yaml_parser as yp
 import pele_platform.Checker.valid_flags as vf
 import pele_platform.main as main
+from test_adaptive import check_file
 
 test_path = os.path.join(cs.DIR, "Examples")
 
 ARGS_1 = os.path.join(test_path, "interaction_restrictions/input.yaml")
 ARGS_2 = os.path.join(test_path, "interaction_restrictions/input_error.yaml")
+ARGS_3 = os.path.join(test_path, "interaction_restrictions/input_error_2.yaml")
 PDB_FILE = os.path.join(test_path, "interaction_restrictions/complex.pdb")
 
 EXPECTED_METRICS = os.path.join(
@@ -86,9 +88,17 @@ def test_conditions_to_json():
     assert interaction_restrictions.conditions_to_json() == expected_output.read()
 
 
-def test_SyntaxError_exception():
+def test_SyntaxError_exception_1():
+    check_SyntaxError_exception(ARGS_2)
+
+
+def test_SyntaxError_exception_2():
+    check_SyntaxError_exception(ARGS_3)
+
+
+def check_SyntaxError_exception(file):
     # Parse yaml file
-    yaml_obj = yp.YamlParser(ARGS_2, vf.VALID_FLAGS_PLATFORM)
+    yaml_obj = yp.YamlParser(file, vf.VALID_FLAGS_PLATFORM)
     yaml_obj.read()
     # Build interaction restrictions
     interaction_restrictions = ir.InteractionRestrictionsBuilder()
@@ -96,13 +106,3 @@ def test_SyntaxError_exception():
         interaction_restrictions.parse_interaction_restrictions(
             PDB_FILE, yaml_obj.interaction_restrictions
         )
-
-
-def check_file(folder, filename, values, errors):
-    filename = os.path.join(folder, filename)
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        for value in values:
-            if value not in "".join(lines):
-                errors.append(value)
-    return errors
