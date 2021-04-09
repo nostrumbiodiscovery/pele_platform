4ae2684ec71863893843c1a07e83492d5d36d24e
Martí Municoy <martimunicoy@gmail.com>
2021-04-10 01:03:26 +0200

    Hot fix for top clusters criterion parameter

--- pele_platform/Utilities/Parameters/SimulationParams/simulation_params.py
+++ pele_platform/Utilities/Parameters/SimulationParams/simulation_params.py
@@ -494,7 +494,7 @@ class SimulationParams(
         self.min_population = args.min_population if args.min_population is not None else 0.01
         self.max_top_clusters = args.max_top_clusters if args.max_top_clusters is not None else 8
         self.max_top_poses = args.max_top_poses if args.max_top_poses is not None else 100
-        self.top_clusters_criterion = args.top_clusters_criterion if args.cluster_conditions is not None else "interaction_25_percentile"
+        self.top_clusters_criterion = args.top_clusters_criterion if args.top_clusters_criterion is not None else "interaction_25_percentile"
         self.cluster_representatives_criterion = args.cluster_representatives_criterion if args.cluster_representatives_criterion is not None else "interaction_5_percentile"
 
     def constraints_params(self, args):
