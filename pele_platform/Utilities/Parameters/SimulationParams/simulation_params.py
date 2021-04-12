98f53e2c1b36d9c755a3079b5ddd5631ce0e640d
Chari Gago <rosario.gago@nostrumbiodiscovery.com>
2021-04-12 17:21:16 +0200

    PELE-471 Singularity_exec configured in Adaptive

--- pele_platform/Utilities/Parameters/SimulationParams/simulation_params.py
+++ pele_platform/Utilities/Parameters/SimulationParams/simulation_params.py
@@ -272,10 +272,13 @@ class SimulationParams(
             os.path.join(os.path.dirname(os.path.dirname(__file__)), "PeleTemplates")
         )
         self.usesrun = "true" if args.usesrun else "false"
+        args.mpi_params = (args.singularity_exec if args.singularity_exec else args.mpi_params)
         mpi_params_name = ("srunParameters" if args.usesrun else "mpiParameters")
         self.mpi_params = (
             f'"{mpi_params_name}": "{args.mpi_params}",' if args.mpi_params else ""
         )
+        if (args.singularity_exec):
+            args.pele_exec = "Pele_mpi"
 
     def optative_params(self, args):
         self.forcefield = (
