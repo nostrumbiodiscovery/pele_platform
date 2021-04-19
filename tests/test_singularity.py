93338977c8a8ecc5e9dcdbcc3c51d4d25cfa69c3
Chari Gago <rosario.gago@nostrumbiodiscovery.com>
2021-04-19 12:46:00 +0200

    PELE-471 More readable tests

--- tests/test_singularity.py
+++ tests/test_singularity.py
@@ -13,14 +13,15 @@ test_path = os.path.join(cs.DIR, "Examples")
 ARGS_1 = os.path.join(test_path, "singularity/input_adaptive.yaml")
 ARGS_2 = os.path.join(test_path, "singularity/input_frag.yaml")
 
-ADAPTIVE_PELE_EXEC = 'Pele_mpi'
+ADAPTIVE_PELE_EXEC = "Pele_mpi"
 ADAPTIVE_MPI_PARAMS = '"mpiParameters": "/path/to/singularity_container.sif",'
-FRAG_PELE_EXEC = '/path/to/singularity_container.sif Pele_mpi'
+FRAG_PELE_EXEC = "/path/to/singularity_container.sif Pele_mpi"
 
 testdata = [
     (ARGS_1, True, ADAPTIVE_PELE_EXEC, ADAPTIVE_MPI_PARAMS),
     (ARGS_2, False, FRAG_PELE_EXEC, None),
 ]
+
 
 def test_singularity_exec(ext_args=ARGS_1):
     """
@@ -39,6 +40,7 @@ def test_singularity_exec(ext_args=ARGS_1):
 
     with pytest.raises(ce.ExecutableNotInPath):
         ck.check_executable_and_env_variables(params)
+
 
 @pytest.mark.parametrize("ext_args, check_mpi, expected1, expected2", testdata)
 def test_singularity_params(ext_args, check_mpi, expected1, expected2):
@@ -61,19 +63,10 @@ def test_singularity_params(ext_args, check_mpi, expected1, expected2):
     builder = ParametersBuilder()
     simulation_params = builder.build_adaptive_variables(params)
 
-    errors = []
-
-    if not simulation_params.pele_exec ==  expected1 :
-        errors.append(
-            f"conditions assert: [{simulation_params.pele_exec}] == [{expected1}] "
-        )
-
-    if check_mpi and not simulation_params.mpi_params == expected2:
-        errors.append(
-            f"conditions assert: [{simulation_params.mpi_params}] == [{expected2}] "
-        )
-
-    assert not errors
+    # Check parameters
+    assert simulation_params.pele_exec == expected1
+    if check_mpi:
+        assert simulation_params.mpi_params == expected2
 
 
 def _read_args(file):
