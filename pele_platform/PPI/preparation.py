486df693a0761f097c969c7361bd8dea66b5c7ad
Anna Gruszka <frumpowy@gmail.com>
2021-05-20 15:38:43 +0200

    PELE-489 Fixed prepwizard command in PPI

--- pele_platform/PPI/preparation.py
+++ pele_platform/PPI/preparation.py
@@ -4,7 +4,7 @@ import subprocess
 from pele_platform.constants import constants
 
 
-def prepare_structure(protein_file, ligand_pdb, chain, remove_water=False):
+def prepare_structure(protein_file, ligand_pdb, chain, remove_water=False, peleffy=False):
     
     to_remove = []
 
@@ -41,9 +41,10 @@ def prepare_structure(protein_file, ligand_pdb, chain, remove_water=False):
         for line in ligand:
             file.write(line)
 
-    # Run through Schrodinger again to add CONECT lines.
-    schrodinger_path = os.path.join(constants.SCHRODINGER, "utilities/prepwizard")
-    command_pdb = f"{schrodinger_path} -nohtreat -noepik -noprotassign -noimpref -noccd -NOJOBID {protein} {new_protein_file}"
-    subprocess.call(command_pdb.split(), shell=True)
+    if peleffy:
+        # Run through Schrodinger again to add CONECT lines.
+        schrodinger_path = os.path.join(constants.SCHRODINGER, "utilities/prepwizard")
+        command_pdb = f"{schrodinger_path} -nohtreat -noepik -noprotassign -noimpref -noccd -NOJOBID {protein_file} {new_protein_file}"
+        subprocess.call(command_pdb.split(), shell=True)
 
     return new_protein_file
