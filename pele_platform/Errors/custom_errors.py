4cc6d8fa8d4f8df9902d2bb33e4c2f5df287b820
Anna Gruszka <frumpowy@gmail.com>
2021-06-10 15:37:26 +0200

    PELE-500 Checker for PELE version and clean-up

--- pele_platform/Errors/custom_errors.py
+++ pele_platform/Errors/custom_errors.py
@@ -1,72 +1,94 @@
-
-
-
-
 class LigandSizeExceed(Exception):
     pass
+
 
 class MissResidueFlag(Exception):
     pass
 
+
 class SameMolecule(Exception):
     pass
+
 
 class ExecutableNotInPath(Exception):
     pass
 
+
 class OldPythonVersion(Exception):
     pass
+
 
 class LigandPreparationError(Exception):
     pass
 
+
 class NoGeometryAroundMetal(Exception):
     pass
+
 
 class EnvVariableNotFound(Exception):
     pass
 
+
 class WrongAtomSpecified(Exception):
     pass
+
 
 class NotCenterOfWaterBox(Exception):
     pass
 
+
 class OutInError(Exception):
     pass
+
 
 class TemplateFileNotFound(Exception):
     pass
 
+
 class RotamersFileNotFound(Exception):
     pass
+
 
 class WrongAtomStringFormat(Exception):
     pass
 
+
 class WrongYamlFile(Exception):
     pass
+
 
 class LigandNameNotSupported(Exception):
     pass
 
+
 class SubstructureError(Exception):
     pass
+
 
 class ProtonationError(Exception):
     pass
 
+
 class ConnectionsError(Exception):
     pass
+
 
 class IncompatibleForcefield(Exception):
     pass
 
+
 class MultipleSimulationTypes(Exception):
     pass
 
+
 class ResidueNotFound(Exception):
     pass
-  
+
+
 class MissingWorkingDir(Exception):
+    pass
+
+
+class IncompatiblePELEVersion(Exception):
     pass
